03a56e53cfb40bbef8408a90753beb8bc55a8989
Unknown <unknown@apache.org>
2010-06-10 11:13:44 +0000

    ConnectorInstance basic integration
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1246078 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/syncope/core/persistence/beans/ConnectorInstance.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright 2010 fabio.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.core.persistence.beans;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author fabio
+ */
+@Entity
+public class ConnectorInstance implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String minorVersion;
+
+    public String getMajorVersion() {
+        return majorVersion;
+    }
+
+    public void setMajorVersion(String majorVersion) {
+        this.majorVersion = majorVersion;
+    }
+
+    public String getMinorVersion() {
+        return minorVersion;
+    }
+
+    public void setMinorVersion(String minorVersion) {
+        this.minorVersion = minorVersion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getXmlConfiguration() {
+        return xmlConfiguration;
+    }
+
+    public void setXmlConfiguration(String xmlConfiguration) {
+        this.xmlConfiguration = xmlConfiguration;
+    }
+    private String majorVersion;
+
+    private String xmlConfiguration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
