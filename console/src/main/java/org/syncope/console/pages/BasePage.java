bd650b5ed74324ecdeeeb737d6322638d77ea9b8
Unknown <unknown@apache.org>
2010-06-17 12:52:19 +0000

    Reload Login page switching language and code review
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1246096 13f79535-47bb-0310-9956-ffa450edef68

--- console/src/main/java/org/syncope/console/pages/BasePage.java
+++ console/src/main/java/org/syncope/console/pages/BasePage.java
@@ -1,3 +1,17 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
 package org.syncope.console.pages;
 
 import org.apache.wicket.PageParameters;
@@ -8,7 +22,6 @@ import org.syncope.console.SyncopeUser;
 
 /**
  * Syncope Wicket base-page.
- * @author lbrandolini
  */
 public class BasePage extends WebPage
 {
@@ -18,12 +31,11 @@ public class BasePage extends WebPage
     /**
      * Constructor that is invoked when page is invoked without a sessadd(new BookmarkablePageLink("roles", Roles.class));ion.
      *
-     * @param parameters
-     *            Page parameters
+     * @param PageParameters parameters
+     *            
      */
     public BasePage( final PageParameters parameters )
     {
-
         add(new BookmarkablePageLink("users", Users.class));
 
         add(new BookmarkablePageLink("roles", Roles.class));
--- console/src/main/java/org/syncope/console/pages/Login.java
+++ console/src/main/java/org/syncope/console/pages/Login.java
@@ -1,12 +1,24 @@
-package org.syncope.console.pages;
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
 
+package org.syncope.console.pages;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
@@ -14,28 +26,23 @@ import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 
 /**
  * Syncope Login page.
- * @author lbrandolini
  */
-public class Login extends WebPage
-{
-    
-public Form form;
-public TextField usernameField;
-public TextField passwordField;
-private List languages = Arrays.asList(new Locale[] { Locale.ENGLISH, Locale.ITALIAN});
-public Locale selected;
+public class Login extends WebPage {
 
+    public Form form;
+    public TextField usernameField;
+    public TextField passwordField;
+    public DropDownChoice<String> languageSelect;
 
-    public Login( PageParameters parameters )
-    {
-        super( parameters );
+    public Login(PageParameters parameters) {
+        super(parameters);
 
-        form = new Form( "login" );
+        form = new Form("login");
 
         usernameField = new TextField("username", new Model());
         usernameField.setMarkupId("username");
@@ -44,38 +51,64 @@ public Locale selected;
         passwordField = new PasswordTextField("password", new Model());
         passwordField.setMarkupId("password");
         form.add(passwordField);
-        
 
-        final DropDownChoice<String> language = new DropDownChoice<String>("language", new PropertyModel<String>(this, "selected"), languages);
-        form.add(language);
+        languageSelect = new LocaleDropDown("language", Arrays
+        .asList(new Locale[] { Locale.ENGLISH, Locale.ITALIAN }));
 
-        language.add(new AjaxFormComponentUpdatingBehavior("onchange")
-        {
-            @Override
-            protected void onUpdate(AjaxRequestTarget target)
-            {
-                if(language.getDefaultModelObject()!=null){
-                getSession().setLocale((Locale)language.getDefaultModelObject());
-                }
-            }
-        });
+        form.add(languageSelect);
 
-        Button submitButton = new Button("submit",new Model(getString("submit")))
-        {
-
+        Button submitButton = new Button("submit", new Model(getString("submit"))) {
             @Override
             public void onSubmit() {
-               System.out.println("Submit");
-               setResponsePage(new HomePage(null));
+                System.out.println("Submit");
+                setResponsePage(new HomePage(null));
             }
-            
         };
 
         submitButton.setDefaultFormProcessing(false);
         form.add(submitButton);
 
-        form.add(new Button("reset",new Model(getString("reset"))));
+        form.add(new Button("reset", new Model(getString("reset"))));
 
         add(form);
+    }
+
+    /**
+     * Inner class which implements (custom) Locale DropDownChoice component.
+     */
+    public class LocaleDropDown extends DropDownChoice {
+
+        private class LocaleRenderer extends ChoiceRenderer {
+
+            @Override
+            public String getDisplayValue(Object locale) {
+                return ((Locale) locale).getDisplayName(getLocale());
+            }
+        }
+
+        public LocaleDropDown(String id, List<Locale> supportedLocales) {
+            super(id, supportedLocales);
+            setChoiceRenderer(new LocaleRenderer());
+            setModel(new IModel() {
+
+                public Object getObject() {
+
+                    return getSession().getLocale();
+
+                }
+
+                public void setObject(Object object) {
+                    getSession().setLocale((Locale) object);
+                }
+
+                public void detach() {
+                }
+            });
+        }
+
+        @Override
+        protected boolean wantOnSelectionChangedNotifications() {
+            return true;
+        }
     }
 }
