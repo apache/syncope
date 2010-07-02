4430f914207a1c248127062a9bd62db92b4caacf
Unknown <unknown@apache.org>
2010-07-02 15:56:39 +0000

    First attempt to include workflow definitions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1246332 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/syncope/core/persistence/SpringHibernateJPAWorkflowStore.java
@@ -0,0 +1,41 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.core.persistence;
+
+import com.opensymphony.workflow.StoreException;
+import com.opensymphony.workflow.spi.hibernate3.AbstractHibernateWorkflowStore;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
+public class SpringHibernateJPAWorkflowStore
+        extends AbstractHibernateWorkflowStore {
+
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
+
+    @Override
+    public void init(Map props) throws StoreException {
+    }
+
+    @Override
+    protected Object execute(InternalCallback action) throws StoreException {
+        return action.doInHibernate((Session) entityManager.getDelegate());
+    }
+}
