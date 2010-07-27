c2d2be66e63ab59f5f57b1a6dd7cb7f4720ce0e9
Unknown <unknown@apache.org>
2010-07-27 09:34:02 +0000

    Very little cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1246416 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/syncope/core/persistence/dao/impl/AbstractDAOImpl.java
+++ core/src/main/java/org/syncope/core/persistence/dao/impl/AbstractDAOImpl.java
@@ -21,13 +21,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.syncope.core.persistence.dao.DAO;
-import org.syncope.core.workflow.JPAPropertySet;
 
 @Configurable
 public abstract class AbstractDAOImpl implements DAO {
 
     protected static final Logger log =
-            LoggerFactory.getLogger(JPAPropertySet.class);
+            LoggerFactory.getLogger(AbstractDAOImpl.class);
     @PersistenceContext(type = PersistenceContextType.EXTENDED)
     protected EntityManager entityManager;
 
