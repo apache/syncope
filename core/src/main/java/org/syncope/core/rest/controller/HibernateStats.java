4eb90b200445456e3482e305aebda6e63951289a
Unknown <unknown@apache.org>
2010-12-13 14:15:20 +0000

    Adding web page for Hibernate statistics
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1246694 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/syncope/core/rest/controller/HibernateStats.java
@@ -0,0 +1,44 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.core.rest.controller;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hibernate")
+public class HibernateStats {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @RequestMapping(method = RequestMethod.GET,
+    value = "/stats")
+    public ModelAndView stats(final HttpServletRequest request) {
+
+        SessionFactory sessionFactory =
+                ((Session) entityManager.getDelegate()).getSessionFactory();
+
+        return new ModelAndView("hibernateStats").addObject("sessionFactory",
+                sessionFactory);
+    }
+}
