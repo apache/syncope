2b8c2f22381f211feea8e82da3c73c764690ec7b
Unknown <unknown@apache.org>
2011-01-04 09:48:58 +0000

    Sequence-based id generation for SyncopeRole
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1246714 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/syncope/core/persistence/beans/role/SyncopeRole.java
+++ core/src/main/java/org/syncope/core/persistence/beans/role/SyncopeRole.java
@@ -22,8 +22,6 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
@@ -51,7 +49,6 @@ import org.hibernate.validator.constraints.Range;
 public class SyncopeRole extends AbstractAttributable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
