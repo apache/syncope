a189f1e7b7d30482e53d213e9e81e30eece225dd
Unknown <unknown@apache.org>
2011-06-22 15:57:11 +0000

    Fixes issue 112
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/branches/0_5_X@1246983 13f79535-47bb-0310-9956-ffa450edef68

---
+++ client/src/main/java/org/syncope/client/search/ResourceCond.java
@@ -0,0 +1,41 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.client.search;
+
+import org.syncope.client.AbstractBaseBean;
+
+/**
+ * Search condition to be applied when searching for associated resources.
+ */
+public class ResourceCond extends AbstractBaseBean {
+
+    private String name;
+
+    public ResourceCond() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public final boolean checkValidity() {
+        return name != null;
+    }
+}
