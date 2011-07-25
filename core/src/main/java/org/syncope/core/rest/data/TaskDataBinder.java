b9bb64fe180a0f28ade487385cf48a1f9da6f919
Unknown <unknown@apache.org>
2011-07-25 10:07:05 +0000

    Missing commit...
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1247048 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/syncope/core/rest/data/TaskDataBinder.java
+++ core/src/main/java/org/syncope/core/rest/data/TaskDataBinder.java
@@ -14,23 +14,38 @@
  */
 package org.syncope.core.rest.data;
 
+import java.util.Date;
 import java.util.List;
+import javassist.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.syncope.client.to.PropagationTaskTO;
+import org.syncope.client.to.SchedTaskTO;
 import org.syncope.client.to.SyncTaskTO;
 import org.syncope.client.to.TaskExecTO;
 import org.syncope.client.to.TaskTO;
 import org.syncope.core.persistence.beans.PropagationTask;
+import org.syncope.core.persistence.beans.SchedTask;
 import org.syncope.core.persistence.beans.SyncTask;
 import org.syncope.core.persistence.beans.TargetResource;
 import org.syncope.core.persistence.beans.Task;
 import org.syncope.core.persistence.beans.TaskExec;
 import org.syncope.core.persistence.beans.role.SyncopeRole;
+import org.syncope.core.persistence.dao.ResourceDAO;
+import org.syncope.core.persistence.dao.RoleDAO;
 import org.syncope.core.util.TaskUtil;
 
 @Component
 public class TaskDataBinder {
+
+    /**
+     * Logger.
+     */
+    protected static final Logger LOG = LoggerFactory.getLogger(
+            TaskDataBinder.class);
 
     private static final String[] IGNORE_TASK_PROPERTIES = {
         "executions", "resource", "defaultResources", "defaultRoles",
@@ -39,9 +54,59 @@ public class TaskDataBinder {
     private static final String[] IGNORE_TASK_EXECUTION_PROPERTIES = {
         "task"};
 
-    public TaskExecTO getTaskExecutionTO(
-            final TaskExec execution) {
+    @Autowired
+    private ResourceDAO resourceDAO;
 
+    @Autowired
+    private RoleDAO roleDAO;
+
+    public SchedTask getSchedTask(final SchedTaskTO taskTO,
+            final TaskUtil taskUtil)
+            throws NotFoundException {
+
+        SchedTask task = taskUtil.newTask();
+        task.setCronExpression(taskTO.getCronExpression());
+
+        if (task instanceof SchedTask) {
+            task.setJobClassName(taskTO.getJobClassName());
+        }
+        if (task instanceof SyncTask) {
+            SyncTaskTO syncTaskTO = (SyncTaskTO) taskTO;
+
+            TargetResource resource = resourceDAO.find(syncTaskTO.getResource());
+            if (resource == null) {
+                throw new NotFoundException("Resource "
+                        + syncTaskTO.getResource());
+            }
+            ((SyncTask) task).setResource(resource);
+
+            for (String resourceName : syncTaskTO.getDefaultResources()) {
+                resource = resourceDAO.find(resourceName);
+                if (resource == null) {
+                    LOG.warn("Ignoring invalid resource " + resourceName);
+                } else {
+                    ((SyncTask) task).addDefaultResource(resource);
+                }
+            }
+
+            SyncopeRole role;
+            for (Long roleId : syncTaskTO.getDefaultRoles()) {
+                role = roleDAO.find(roleId);
+                if (role == null) {
+                    LOG.warn("Ignoring invalid role " + roleId);
+                } else {
+                    ((SyncTask) task).addDefaultRole(role);
+                }
+            }
+
+            ((SyncTask) task).setUpdateIdentities(
+                    syncTaskTO.isUpdateIdentities());
+        }
+
+        return task;
+    }
+
+    public TaskExecTO getTaskExecutionTO(final TaskExec execution) {
         TaskExecTO executionTO = new TaskExecTO();
         BeanUtils.copyProperties(execution, executionTO,
                 IGNORE_TASK_EXECUTION_PROPERTIES);
@@ -66,9 +131,14 @@ public class TaskDataBinder {
                 break;
 
             case SCHED:
+                ((SchedTaskTO) taskTO).setLastExec(new Date());
+                ((SchedTaskTO) taskTO).setNextExec(new Date());
                 break;
 
             case SYNC:
+                ((SyncTaskTO) taskTO).setLastExec(new Date());
+                ((SyncTaskTO) taskTO).setNextExec(new Date());
+
                 ((SyncTaskTO) taskTO).setResource(
                         ((SyncTask) task).getResource().getName());
                 for (TargetResource resource :
