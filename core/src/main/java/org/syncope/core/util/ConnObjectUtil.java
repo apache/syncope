da86f70159833936b09c5d0940414e519ac1d43a
Fabio Martelli <fmartelli@apache.org>
2012-03-02 14:28:31 +0000

    missed class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/syncope/trunk@1296225 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/syncope/core/util/ConnObjectUtil.java
@@ -0,0 +1,361 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.syncope.core.util;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import javassist.NotFoundException;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
+import org.identityconnectors.common.security.GuardedByteArray;
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.ConnectorObject;
+import org.identityconnectors.framework.common.objects.OperationalAttributes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.syncope.client.mod.AttributeMod;
+import org.syncope.client.mod.UserMod;
+import org.syncope.client.to.AbstractAttributableTO;
+import org.syncope.client.to.AttributeTO;
+import org.syncope.client.to.ConnObjectTO;
+import org.syncope.client.to.MembershipTO;
+import org.syncope.client.to.UserTO;
+import org.syncope.core.persistence.beans.SchemaMapping;
+import org.syncope.core.persistence.beans.SyncTask;
+import org.syncope.core.rest.controller.UnauthorizedRoleException;
+import org.syncope.core.rest.data.UserDataBinder;
+
+@Component
+public class ConnObjectUtil {
+
+    /**
+     * Logger.
+     */
+    protected static final Logger LOG = LoggerFactory.getLogger(ConnObjectUtil.class);
+
+    /**
+     * JEXL engine for evaluating connector's account link.
+     */
+    @Autowired
+    private JexlUtil jexlUtil;
+
+    /**
+     * User data binder.
+     */
+    @Autowired
+    private UserDataBinder userDataBinder;
+
+    /**
+     * Build an UserTO out of connector object attributes and schema mapping.
+     *
+     * @param obj connector object
+     * @return UserTO for the user to be created
+     */
+    public UserTO getUserTO(final ConnectorObject obj, final SyncTask syncTask) {
+
+        final UserTO userTO = new UserTO();
+
+        // 1. fill with data from connector object
+        for (SchemaMapping mapping : syncTask.getResource().getMappings()) {
+            Attribute attribute = obj.getAttributeByName(SchemaMappingUtil.getExtAttrName(mapping));
+
+            AttributeTO attributeTO;
+            switch (mapping.getIntMappingType()) {
+                case SyncopeUserId:
+                    break;
+
+                case Password:
+                    if (attribute != null && attribute.getValue() != null && !attribute.getValue().isEmpty()) {
+                        userTO.setPassword(getPassword(attribute.getValue().get(0)));
+                    }
+                    break;
+
+                case Username:
+                    userTO.setUsername(attribute == null || attribute.getValue().isEmpty()
+                            ? null : attribute.getValue().get(0).toString());
+                    break;
+
+                case UserSchema:
+                    attributeTO = new AttributeTO();
+                    attributeTO.setSchema(mapping.getIntAttrName());
+
+                    for (Object value : attribute == null ? Collections.EMPTY_LIST : attribute.getValue()) {
+                        attributeTO.addValue(value.toString());
+                    }
+
+                    userTO.addAttribute(attributeTO);
+                    break;
+
+                case UserDerivedSchema:
+                    attributeTO = new AttributeTO();
+                    attributeTO.setSchema(mapping.getIntAttrName());
+                    userTO.addDerivedAttribute(attributeTO);
+                    break;
+
+                case UserVirtualSchema:
+                    attributeTO = new AttributeTO();
+                    attributeTO.setSchema(mapping.getIntAttrName());
+                    userTO.addVirtualAttribute(attributeTO);
+                    break;
+
+                default:
+            }
+        }
+
+        // 2. add data from defined template (if any)
+        UserTO template = syncTask.getUserTemplate();
+        if (template != null) {
+            if (StringUtils.isBlank(userTO.getUsername()) && StringUtils.isNotBlank(template.getUsername())) {
+                String evaluated = jexlUtil.evaluate(template.getUsername(), userTO);
+                if (StringUtils.isNotBlank(evaluated)) {
+                    userTO.setUsername(template.getUsername());
+                }
+            }
+
+            if (StringUtils.isBlank(userTO.getPassword()) && StringUtils.isNotBlank(template.getPassword())) {
+                String evaluated = jexlUtil.evaluate(template.getPassword(), userTO);
+                if (StringUtils.isNotBlank(evaluated)) {
+                    userTO.setPassword(template.getPassword());
+                }
+            }
+
+            fillFromTemplate(userTO, template);
+
+            for (String resource : template.getResources()) {
+                userTO.addResource(resource);
+            }
+
+            Map<Long, MembershipTO> currentMembs = userTO.getMembershipMap();
+            for (MembershipTO membTO : template.getMemberships()) {
+                MembershipTO membTBU;
+                if (currentMembs.containsKey(membTO.getRoleId())) {
+                    membTBU = currentMembs.get(membTO.getRoleId());
+                } else {
+                    membTBU = new MembershipTO();
+                    membTBU.setRoleId(membTO.getRoleId());
+                    userTO.addMembership(membTBU);
+                }
+                fillFromTemplate(membTBU, membTO);
+            }
+        }
+
+        // 3. if password was not set above, generate a random string
+        if (StringUtils.isBlank(userTO.getPassword())) {
+            userTO.setPassword(RandomStringUtils.randomAlphanumeric(16));
+        }
+
+        return userTO;
+    }
+
+    /**
+     * Build an UserMod out of connector object attributes and schema mapping.
+     *
+     * @param userId user to be updated
+     * @param obj connector object
+     * @return UserMod for the user to be updated
+     */
+    public UserMod getUserMod(final Long userId, final ConnectorObject obj, final SyncTask syncTask) {
+
+        final UserMod userMod = new UserMod();
+        userMod.setId(userId);
+
+        for (SchemaMapping mapping : syncTask.getResource().getMappings()) {
+            Attribute attribute = obj.getAttributeByName(SchemaMappingUtil.getExtAttrName(mapping));
+
+            List<Object> values = attribute == null
+                    ? Collections.EMPTY_LIST : attribute.getValue();
+
+            AttributeMod attributeMod;
+            switch (mapping.getIntMappingType()) {
+                case SyncopeUserId:
+                    break;
+
+                case Password:
+                    attribute = obj.getAttributeByName(
+                            OperationalAttributes.PASSWORD_NAME);
+
+                    if (attribute != null && attribute.getValue() != null
+                            && !attribute.getValue().isEmpty()) {
+
+                        String password =
+                                getPassword(attribute.getValue().get(0));
+                        // update password if and only if password has really 
+                        // changed
+                        try {
+                            if (!userDataBinder.verifyPassword(userId,
+                                    password)) {
+
+                                userMod.setPassword(password);
+                            }
+                        } catch (NotFoundException e) {
+                            LOG.error("Could not find user {}", userId, e);
+                        } catch (UnauthorizedRoleException e) {
+                            LOG.error("Not allowed to read user {}", userId, e);
+                        }
+                    }
+                    break;
+
+                case Username:
+                    if (values != null && !values.isEmpty()) {
+                        userMod.setUsername(values.get(0).toString());
+                    }
+                    break;
+
+                case UserSchema:
+                    userMod.addAttributeToBeRemoved(mapping.getIntAttrName());
+
+                    attributeMod = new AttributeMod();
+                    attributeMod.setSchema(mapping.getIntAttrName());
+                    for (Object value : values) {
+                        attributeMod.addValueToBeAdded(value.toString());
+                    }
+                    userMod.addAttributeToBeUpdated(attributeMod);
+                    break;
+
+                case UserDerivedSchema:
+                    userMod.addDerivedAttributeToBeAdded(mapping.getIntAttrName());
+                    break;
+
+                case UserVirtualSchema:
+                    userMod.addVirtualAttributeToBeRemoved(mapping.getIntAttrName());
+
+                    attributeMod = new AttributeMod();
+                    attributeMod.setSchema(mapping.getIntAttrName());
+                    for (Object value : values) {
+                        attributeMod.addValueToBeAdded(value.toString());
+                    }
+                    userMod.addVirtualAttributeToBeUpdated(attributeMod);
+                    break;
+
+                default:
+            }
+        }
+
+        return userMod;
+    }
+
+    /**
+     * Extract password value from passed value (if instance of GuardedString or GuardedByteArray).
+     *
+     * @param pwd received from the underlying connector
+     * @return password value
+     */
+    public String getPassword(final Object pwd) {
+        final StringBuilder result = new StringBuilder();
+
+        if (pwd instanceof GuardedString) {
+            ((GuardedString) pwd).access(new GuardedString.Accessor() {
+
+                @Override
+                public void access(final char[] clearChars) {
+                    result.append(clearChars);
+                }
+            });
+        } else if (pwd instanceof GuardedByteArray) {
+            ((GuardedByteArray) pwd).access(new GuardedByteArray.Accessor() {
+
+                @Override
+                public void access(final byte[] clearBytes) {
+                    result.append(new String(clearBytes));
+                }
+            });
+        } else if (pwd instanceof String) {
+            result.append((String) pwd);
+        } else {
+            result.append(pwd.toString());
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Get connector object TO from a connector object.
+     *
+     * @param connObject connector object.
+     * @return connector object TO.
+     */
+    public ConnObjectTO getConnObjectTO(final ConnectorObject connObject) {
+        final ConnObjectTO connObjectTO = new ConnObjectTO();
+
+        for (Attribute attr : connObject.getAttributes()) {
+            AttributeTO attrTO = new AttributeTO();
+            attrTO.setSchema(attr.getName());
+
+            if (attr.getValue() != null) {
+                for (Object value : attr.getValue()) {
+                    if (value != null) {
+                        attrTO.addValue(value.toString());
+                    }
+                }
+            }
+
+            connObjectTO.addAttribute(attrTO);
+        }
+
+        return connObjectTO;
+    }
+
+    private void fillFromTemplate(final AbstractAttributableTO attributableTO, final AbstractAttributableTO template) {
+
+        Map<String, AttributeTO> currentAttrMap =
+                attributableTO.getAttributeMap();
+        for (AttributeTO attrTO : template.getAttributes()) {
+            if (!currentAttrMap.containsKey(attrTO.getSchema())) {
+                attributableTO.addAttribute(evaluateAttrTemplate(attributableTO, attrTO));
+            }
+        }
+
+        currentAttrMap = attributableTO.getDerivedAttributeMap();
+        for (AttributeTO attrTO : template.getDerivedAttributes()) {
+            if (!currentAttrMap.containsKey(attrTO.getSchema())) {
+                attributableTO.addDerivedAttribute(attrTO);
+            }
+        }
+
+        currentAttrMap = attributableTO.getVirtualAttributeMap();
+        for (AttributeTO attrTO : template.getDerivedAttributes()) {
+            if (!currentAttrMap.containsKey(attrTO.getSchema())) {
+                attributableTO.addVirtualAttribute(evaluateAttrTemplate(attributableTO, attrTO));
+            }
+        }
+    }
+
+    private AttributeTO evaluateAttrTemplate(
+            final AbstractAttributableTO attributableTO,
+            final AttributeTO template) {
+
+        AttributeTO result = new AttributeTO();
+        result.setSchema(template.getSchema());
+
+        if (template.getValues() != null && !template.getValues().isEmpty()) {
+            for (String value : template.getValues()) {
+                String evaluated = jexlUtil.evaluate(value, attributableTO);
+                if (StringUtils.isNotBlank(evaluated)) {
+                    result.addValue(evaluated);
+                }
+            }
+        }
+
+        return result;
+    }
+}
