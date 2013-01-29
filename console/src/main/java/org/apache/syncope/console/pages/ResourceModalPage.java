08b2a6726055dd6f74c004df5e5dbda6aa7db9c4
Colm O Heigeartaigh <coheigea@apache.org>
2013-01-29 11:06:26 +0000

    Minor tweak to last commit
    git-svn-id: https://svn.apache.org/repos/asf/syncope/trunk@1439832 13f79535-47bb-0310-9956-ffa450edef68

--- console/src/main/java/org/apache/syncope/console/pages/ResourceModalPage.java
+++ console/src/main/java/org/apache/syncope/console/pages/ResourceModalPage.java
@@ -129,8 +129,7 @@ public class ResourceModalPage extends BaseModalPage {
                         ((Resources) pageref.getPage()).setModalResult(true);
                         window.close(target);
                     } catch (Exception e) {
-                        LOG.error("Failure managing resource {}", resourceTO);
-                        LOG.error("Resource Management Exception:", e);
+                        LOG.error("Failure managing resource {}", resourceTO, e);
                         error(new ResourceModel("error", "error").getObject() + ":" + e.getMessage());
                         target.add(feedbackPanel);
                     }
