e6526520590a3dc315c82f07f6e3f1cd30c9826f
Andrei Shakirin <ashakirin@apache.org>
2013-02-01 12:38:39 +0000

    [SYNCOPE-298] Workaround for Id conflicts in integration tests
    git-svn-id: https://svn.apache.org/repos/asf/syncope/trunk@1441426 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/syncope/core/persistence/dao/VirAttrTest.java
+++ core/src/test/java/org/apache/syncope/core/persistence/dao/VirAttrTest.java
@@ -63,7 +63,7 @@ public class VirAttrTest extends AbstractDAOTest {
 
     @Test
     public void findById() {
-        UVirAttr attribute = virAttrDAO.find(100L, UVirAttr.class);
+        UVirAttr attribute = virAttrDAO.find(1000L, UVirAttr.class);
         assertNotNull("did not find expected attribute schema", attribute);
     }
 
@@ -128,12 +128,12 @@ public class VirAttrTest extends AbstractDAOTest {
 
     @Test
     public void delete() {
-        UVirAttr attribute = virAttrDAO.find(100L, UVirAttr.class);
+        UVirAttr attribute = virAttrDAO.find(1000L, UVirAttr.class);
         String attributeSchemaName = attribute.getVirtualSchema().getName();
 
         virAttrDAO.delete(attribute.getId(), UVirAttr.class);
 
-        UVirAttr actual = virAttrDAO.find(100L, UVirAttr.class);
+        UVirAttr actual = virAttrDAO.find(1000L, UVirAttr.class);
         assertNull("delete did not work", actual);
 
         UVirSchema attributeSchema = virSchemaDAO.find(attributeSchemaName, UVirSchema.class);
