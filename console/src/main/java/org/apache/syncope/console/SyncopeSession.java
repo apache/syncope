81a609118f27e7ed0d3c56bfc5e467af3e0b3187
Francesco Chicchiriccò <ilgrosso@apache.org>
2013-04-12 06:59:30 +0000

    [SYNCOPE-340] Applying provided patch
    git-svn-id: https://svn.apache.org/repos/asf/syncope/branches/1_0_X@1467179 13f79535-47bb-0310-9956-ffa450edef68

--- console/src/main/java/org/apache/syncope/console/SyncopeSession.java
+++ console/src/main/java/org/apache/syncope/console/SyncopeSession.java
@@ -39,7 +39,7 @@ public class SyncopeSession extends WebSession {
     private static final long serialVersionUID = 7743446298924805872L;
 
     public static final List<Locale> SUPPORTED_LOCALES = Arrays.asList(new Locale[] {
-        Locale.ENGLISH, Locale.ITALIAN});
+        Locale.ENGLISH, Locale.ITALIAN, Locale.forLanguageTag("pt-BR")});
 
     private String userId;
 
