cfea4511649744c9088b3aee67a20b81d86625df
Francesco Chicchiriccò <ilgrosso@apache.org>
2013-08-09 16:02:06 +0000

    [SYNCOPE-410] Applying provided patch
    git-svn-id: https://svn.apache.org/repos/asf/syncope/branches/1_1_X@1512363 13f79535-47bb-0310-9956-ffa450edef68

--- common/src/main/java/org/apache/syncope/common/types/AccountPolicySpec.java
+++ common/src/main/java/org/apache/syncope/common/types/AccountPolicySpec.java
@@ -43,6 +43,11 @@ public class AccountPolicySpec extends AbstractPolicySpec {
     private int minLength;
 
     /**
+     * Pattern (regular expression) that must match.
+     */
+    private String pattern;
+
+    /**
      * Substrings not permitted.
      */
     private List<String> wordsNotPermitted;
@@ -117,6 +122,14 @@ public class AccountPolicySpec extends AbstractPolicySpec {
 
     public void setMinLength(final int minLength) {
         this.minLength = minLength;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(final String pattern) {
+        this.pattern = pattern;
     }
 
     @XmlElementWrapper(name = "prefixesNotPermitted")
