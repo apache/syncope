62c50276a1e8abbf944688209a969cee76efc14e
Marco Di Sabatino Di Diodoro <mdisabatino@apache.org>
2016-03-03 16:48:46 +0100

    [SYNCOPE-752] Add console tests for realms

---
+++ fit/core-reference/src/test/java/org/apache/syncope/fit/console/RealmsITCase.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.syncope.fit.console;
+
+import static org.apache.syncope.fit.console.AbstractConsoleITCase.KEY;
+
+import de.agilecoders.wicket.core.markup.html.bootstrap.dialog.Modal;
+import org.apache.syncope.client.console.pages.Realms;
+import org.apache.wicket.Component;
+import org.apache.wicket.util.tester.FormTester;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.JVM)
+public class RealmsITCase extends AbstractConsoleITCase<String> {
+    
+    @Before
+    public void login() {
+        doLogin(ADMIN_UNAME, ADMIN_PWD);
+        wicketTester.clickLink("body:realmsLI:realms");
+        wicketTester.assertRenderedPage(Realms.class);
+    }
+
+    @Test
+    public void read() {
+        wicketTester.assertLabel("body:content:body:tabbedPanel:panel:container:name:field-label", "Name");
+    }
+
+    @Test
+    public void create() {
+        wicketTester.clickLink("body:content:body:tabbedPanel:panel:actions:actions:panelCreate:createLink");
+
+        wicketTester.assertComponent("body:content:modal", Modal.class);
+
+        FormTester formTester = wicketTester.newFormTester("body:content:modal:form");
+        formTester.setValue("content:details:container:name:textField", "testRealm");
+
+        wicketTester.clickLink("body:content:modal:dialog:footer:inputs:0:submit");
+
+        wicketTester.assertInfoMessages("Operation executed successfully");
+        wicketTester.cleanupFeedbackMessages();
+    }
+
+}
