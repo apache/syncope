251b8cbd9ff481319e868a7e3a754329b15dbca0
berezkin <berezkin@berezkin.berezkin>
2016-04-03 14:57:11 +0300

    Russian locale added
    test commit with adding russian locale

--- client/console/src/main/java/org/apache/syncope/client/console/SyncopeConsoleApplication.java
+++ client/console/src/main/java/org/apache/syncope/client/console/SyncopeConsoleApplication.java
@@ -70,7 +70,7 @@ public class SyncopeConsoleApplication extends AuthenticatedWebApplication {
 
     public static final List<Locale> SUPPORTED_LOCALES = Collections.unmodifiableList(Arrays.asList(
             new Locale[] {
-                Locale.ENGLISH, Locale.ITALIAN, new Locale("pt", "BR")
+                Locale.ENGLISH, Locale.ITALIAN, new Locale("pt", "BR"), new Locale("ru")
             }));
 
     private static final String ACTIVITI_MODELER_CONTEXT = "activiti-modeler";
