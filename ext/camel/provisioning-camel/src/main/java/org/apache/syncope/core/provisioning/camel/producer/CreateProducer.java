6ae5020032be616fcdf44510ea0572586cff0814
Colm O hEigeartaigh <coheigea@apache.org>
2016-07-28 14:08:00 +0100

    SYCOPE-910 - Introduce new Camel propagation component - part I

--- ext/camel/provisioning-camel/src/main/java/org/apache/syncope/core/provisioning/camel/producer/CreateProducer.java
+++ ext/camel/provisioning-camel/src/main/java/org/apache/syncope/core/provisioning/camel/producer/CreateProducer.java
@@ -1,27 +1,26 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.apache.syncope.core.provisioning.camel.processor;
+package org.apache.syncope.core.provisioning.camel.producer;
 
 import java.util.List;
 import java.util.Set;
+
+import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.syncope.common.lib.to.AnyObjectTO;
@@ -30,41 +29,36 @@ import org.apache.syncope.common.lib.to.UserTO;
 import org.apache.syncope.common.lib.types.AnyTypeKind;
 import org.apache.syncope.core.persistence.api.entity.task.PropagationTask;
 import org.apache.syncope.core.provisioning.api.WorkflowResult;
-import org.apache.syncope.core.provisioning.api.propagation.PropagationManager;
 import org.apache.syncope.core.provisioning.api.propagation.PropagationReporter;
-import org.apache.syncope.core.provisioning.api.propagation.PropagationTaskExecutor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.apache.syncope.core.provisioning.camel.AnyType;
 
-@Component
-public class CreateProcessor implements Processor {
+public class CreateProducer extends AbstractProducer {
 
-    @Autowired
-    protected PropagationManager propagationManager;
-
-    @Autowired
-    protected PropagationTaskExecutor taskExecutor;
+    public CreateProducer(final Endpoint endpoint, final AnyType anyType) {
+        super(endpoint, anyType);
+    }
 
     @SuppressWarnings("unchecked")
     @Override
-    public void process(final Exchange exchange) {
+    public void process(final Exchange exchange) throws Exception {
         if ((exchange.getIn().getBody() instanceof WorkflowResult)) {
             Object actual = exchange.getProperty("actual");
             Set<String> excludedResources = exchange.getProperty("excludedResources", Set.class);
             Boolean nullPriorityAsync = exchange.getProperty("nullPriorityAsync", Boolean.class);
-            
+
             if (actual instanceof UserTO) {
                 WorkflowResult<Pair<String, Boolean>> created =
                         (WorkflowResult<Pair<String, Boolean>>) exchange.getIn().getBody();
-    
-                List<PropagationTask> tasks = propagationManager.getUserCreateTasks(
+
+                List<PropagationTask> tasks = getPropagationManager().getUserCreateTasks(
                         created.getResult().getKey(),
                         ((UserTO) actual).getPassword(),
                         created.getResult().getValue(),
                         created.getPropByRes(),
                         ((UserTO) actual).getVirAttrs(),
                         excludedResources);
-                PropagationReporter propagationReporter = taskExecutor.execute(tasks, nullPriorityAsync);
+                PropagationReporter propagationReporter = 
+                    getPropagationTaskExecutor().execute(tasks, nullPriorityAsync);
 
                 exchange.getOut().setBody(
                         new ImmutablePair<>(created.getResult().getKey(), propagationReporter.getStatuses()));
@@ -75,13 +69,14 @@ public class CreateProcessor implements Processor {
                 if (actual instanceof AnyObjectTO) {
                     anyTypeKind = AnyTypeKind.ANY_OBJECT;
                 }
-                List<PropagationTask> tasks = propagationManager.getCreateTasks(
+                List<PropagationTask> tasks = getPropagationManager().getCreateTasks(
                         anyTypeKind,
                         created.getResult(),
                         created.getPropByRes(),
                         ((AnyTO) actual).getVirAttrs(),
                         excludedResources);
-                PropagationReporter propagationReporter = taskExecutor.execute(tasks, nullPriorityAsync);
+                PropagationReporter propagationReporter = 
+                    getPropagationTaskExecutor().execute(tasks, nullPriorityAsync);
 
                 exchange.getOut().setBody(new ImmutablePair<>(created.getResult(), propagationReporter.getStatuses()));
             }
