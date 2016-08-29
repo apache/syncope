283b07bd8d7bca7dfb359a2c0fc039b87da81aec
giacomolm <giacomolm@hotmail.it>
2016-08-29 12:46:12 +0200

    Adding protractor configuration

---
+++ client/enduser/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/login.js
@@ -0,0 +1,32 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+describe('syncope enduser login', function() {
+
+  it('login bellini', function() {
+    browser.get('http://localhost:9080/syncope-enduser/app/');
+
+    element(by.model('credentials.username')).sendKeys('bellini');
+    element(by.model('credentials.password')).sendKeys('password');
+    element(by.id('login-btn')).click();
+
+    expect(element(by.model('user.username')).getAttribute('value')).toEqual('bellini');
+  });
+});
+
