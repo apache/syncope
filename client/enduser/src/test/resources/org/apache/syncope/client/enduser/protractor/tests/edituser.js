ac5be5889c26e41f39dc8d660bdd6b4813ead47d
Matteo Di Carlo <matteo.dicarlo@tirasa.net>
2016-09-06 11:31:04 +0200

    [SYNCOPE-853] add angularjs tests for edit-user in enduser component

--- client/enduser/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/edituser.js
+++ client/enduser/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/edituser.js
@@ -17,45 +17,79 @@
  * under the License.
  */
 
-
 describe('syncope enduser user edit', function () {
 
   function next() {
     element.all(by.id('next')).last().click();
-  };
-
-
-  function fillDropDownMenu(modelId, option) {
-    this.item = element(by.model(modelId));
-    this.item.click();
-    element.all(by.id(option)).first().click();
-    browser.driver.sleep(1500);
   }
   ;
 
+  function cancel() {
+    element.all(by.id('cancel')).last().click();
+  }
+  ;
 
   it('should edit user credentials', function () {
     browser.get('http://localhost:9080/syncope-enduser/app/');
 
-    //login
+//login
     element(by.model('credentials.username')).sendKeys('bellini');
     element(by.model('credentials.password')).sendKeys('password');
-    element.all(by.options('language.name for language in languages.availableLanguages track by language.id')).then(function (language) {
-      expect(language.length).toBe(3);
-    });
-    element.all(by.options('language.name for language in languages.availableLanguages track by language.id')).get(0).click();
+    element.all(by.options('language.name for language in languages.availableLanguages track by language.id')).
+            then(function (language) {
+              expect(language.length).toBe(3);
+            });
+    element.all(by.options('language.name for language in languages.availableLanguages track by language.id')).
+            get(0).click();
     element(by.id('login-btn')).click();
 
-//    credential
-//    element(by.model('user.username')).sendKeys('bellini123');
-//    element(by.model('user.password')).sendKeys('password123');
-//    element(by.model('confirmPassword.value')).sendKeys('password123');
+//credential
+    element(by.model('user.username')).clear();
+    element(by.model('user.username')).sendKeys('bellini');
+    element(by.model('user.password')).clear();
+    element(by.model('user.password')).sendKeys('password');
+    element(by.model('confirmPassword.value')).sendKeys('password');
+    var secQuestion = element(by.model('user.securityQuestion'));
+    var selectedSecQuestion = secQuestion.all(by.options('securityQuestion.key as securityQuestion.content for securityQuestion in availableSecurityQuestions')).last();
+    selectedSecQuestion.click();
+    element(by.model('user.securityAnswer')).sendKeys('Agata Ferlito');
+    browser.driver.sleep(1000);
     next();
 
+//groups
+    var group = element(by.model('dynamicForm.selectedGroups'));
+    var selectedGroup = group.element(by.css('.ui-select-search'));
+    group.click();
+    
+    selectedGroup.sendKeys('additional');
+    element.all(by.css('.ui-select-choices-row-inner span')).first().click();
+    selectedGroup.sendKeys('root');
+    element.all(by.css('.ui-select-choices-row-inner span')).first().click();
+    browser.driver.sleep(1500);
+        
+    next();
 
-    fillDropDownMenu('dynamicForm.selectedGroups', 'additional');
-//    fillDropDownMenu('dynamicForm.selectedAuxClasses','csv');
+//plainSchemas
+
+    element(by.css('[name="fullname"]')).clear();
+    element(by.css('[name="fullname"]')).sendKeys('Vincenzo Bellini');
+    element(by.css('[name="userId"]')).clear();
+    element(by.css('[name="userId"]')).sendKeys('bellini@apache.org');
+    var selectedDate = element(by.model('selectedDate')).click();
+    selectedDate.clear();
+    selectedDate.sendKeys('2009-06-21');
+    element(by.css('[name="firstname"]')).clear();
+    element(by.css('[name="firstname"]')).sendKeys('Vincenzo');
+    element(by.css('[name="surname"]')).clear();
+    element(by.css('[name="surname"]')).sendKeys('Bellini');
+    element(by.css('[name="ctype"]')).clear();
+    element(by.css('[name="ctype"]')).sendKeys('bellinictype');
+
+    next();
 
   });
 });
+
+
+
 
