496028e821d0b279c2777aeba680f97f0bb04378
Matteo Di Carlo <matteo.dicarlo@tirasa.net>
2016-09-27 15:27:29 +0200

    [SYNCOPE-973] updates tests

--- fit/enduser-reference/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/create.js
+++ fit/enduser-reference/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/create.js
@@ -20,12 +20,15 @@
 var abstract = require('./abstract.js');
 
 describe('syncope enduser user create', function () {
-
   it('should create user donizetti', function () {
-    console.log("create user test");
+    console.log("");
+    console.log("user create");
     abstract.goHome();
-
+    abstract.waitSpinner();
+    browser.wait(element(by.id('register')).isPresent());
     element(by.id('register')).click();
+
+    browser.wait(element(by.id('user.username')).isPresent());
     element(by.model('user.username')).sendKeys('donizetti');
     element(by.model('user.password')).sendKeys('password123');
     element(by.model('confirmPassword.value')).sendKeys('password123');
@@ -33,20 +36,33 @@ describe('syncope enduser user create', function () {
     element(by.cssContainingText('option', 'What\'s your mother\'s maiden name?')).click();
 
     browser.wait(element(by.id('user.securityAnswer')).isPresent());
-    element(by.model('user.securityAnswer')).sendKeys('Agata Ferlito');
-
-    element.all(by.id('next')).first().click();
+    element(by.model('user.securityAnswer')).sendKeys('Domenica Oliva Nava');
     element.all(by.id('next')).first().click();
 
+    //groups
+    var group = element(by.model('dynamicForm.selectedGroups'));
+    var selectedGroup = group.element(by.css('.ui-select-search'));
+    group.click();
+    //adds group root
+    selectedGroup.sendKeys('root');
+    element.all(by.css('.ui-select-choices-row-inner span')).first().click();
+    abstract.waitSpinner();
+    element.all(by.id('next')).first().click();
+
+    //plainSchemas
     element.all(by.name('fullname')).first().sendKeys('Gaetano Donizetti');
     element.all(by.name('userId')).first().sendKeys('donizetti@apache.org');
     element.all(by.name('firstname')).first().sendKeys('Gaetano');
     element.all(by.name('surname')).first().sendKeys('Donizetti');
+    element.all(by.id('next')).first().click();
 
+    //derivedSchemas
+    element.all(by.id('next')).first().click();
+
+    //virtualSchemas
     element.all(by.id('next')).first().click();
     element.all(by.id('next')).first().click();
-    element.all(by.id('next')).first().click();
-    element.all(by.id('next')).first().click();
+
     element.all(by.id('save')).last().click();
   });
 });
