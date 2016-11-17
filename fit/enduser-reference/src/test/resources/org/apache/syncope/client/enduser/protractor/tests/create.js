86b836b2a5502f0b9277354c3b1f4b4449c05632
Matteo Di Carlo <matteo.dicarlo@tirasa.net>
2016-11-17 14:30:30 +0100

    [SYNCOPE-967] enduser tests updated

--- fit/enduser-reference/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/create.js
+++ fit/enduser-reference/src/test/resources/org/apache/syncope/client/enduser/protractor/tests/create.js
@@ -18,30 +18,28 @@
  */
 
 var abstract = require('./abstract.js');
-
 describe('syncope enduser user create', function () {
   it('should create user donizetti', function () {
     console.log("");
     console.log("user create");
     abstract.goHome();
-    abstract.waitSpinner();
     browser.wait(element(by.id('register')).isPresent());
     element(by.id('register')).click();
 
+    //creadentials
     browser.wait(element(by.id('user.username')).isPresent());
     element(by.model('user.username')).sendKeys('donizetti');
     element(by.model('user.password')).sendKeys('password123');
     element(by.model('confirmPassword.value')).sendKeys('password123');
-
     element(by.cssContainingText('option', 'What\'s your mother\'s maiden name?')).click();
-
     browser.wait(element(by.id('user.securityAnswer')).isPresent());
     element(by.model('user.securityAnswer')).sendKeys('Domenica Oliva Nava');
     element.all(by.id('next')).first().click();
 
     //groups
-    abstract.waitSpinner();
-    browser.wait(element(by.model('dynamicForm.selectedGroups')).isPresent());
+    browser.wait(element(by.model('user.realm')).isPresent());
+    element(by.model('user.realm')).click();
+    element.all(by.repeater('realm in availableRealms')).get(1).click();
     var group = element(by.model('dynamicForm.selectedGroups'));
     var selectedGroup = group.element(by.css('.ui-select-search'));
     group.click();
@@ -60,13 +58,16 @@ describe('syncope enduser user create', function () {
     element.all(by.name('surname')).first().sendKeys('Donizetti');
     element.all(by.id('next')).first().click();
 
-    //derivedSchemas
-    element.all(by.id('next')).first().click();
-
-    //virtualSchemas
-    element.all(by.id('next')).first().click();
-    element.all(by.id('next')).first().click();
-
+    //derivedSchemas,virtualSchemas,resources
+    for (var i = 0; i < 3; i++) {
+      element.all(by.id('next')).first().click();
+    }
+    //finish: breadcrumb should be clickable, testing navigation
+    for (var i = 0; i < 5; i++) {
+      element.all(by.repeater('(key, value) in wizard')).get(i).click();
+      browser.wait(element(by.id('finish')).isPresent());
+      element.all(by.id('finish')).last().click();
+    }
     element.all(by.id('save')).last().click();
   });
 });
