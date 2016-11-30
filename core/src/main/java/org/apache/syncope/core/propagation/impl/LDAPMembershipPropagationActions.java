317f703b8b979d036d2a97b3e471fbea2f12b1e3
Francesco Chicchiriccò <ilgrosso@apache.org>
2016-11-30 15:29:20 +0100

    Avoid duplicate ldapGroups

--- core/src/main/java/org/apache/syncope/core/propagation/impl/LDAPMembershipPropagationActions.java
+++ core/src/main/java/org/apache/syncope/core/propagation/impl/LDAPMembershipPropagationActions.java
@@ -98,14 +98,14 @@ public class LDAPMembershipPropagationActions extends DefaultPropagationActions 
 
                 Set<String> groups = new HashSet<String>(roleAccountLinks);
                 Attribute ldapGroups = AttributeUtil.find(getGroupMembershipAttrName(), attributes);
-
                 if (ldapGroups != null) {
                     for (Object obj : ldapGroups.getValue()) {
                         groups.add(obj.toString());
                     }
+                    attributes.remove(ldapGroups);
                 }
-
                 attributes.add(AttributeBuilder.build(getGroupMembershipAttrName(), groups));
+
                 task.setAttributes(attributes);
             }
         } else {
