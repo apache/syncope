a799dbc0bbcce4635ab41cd8e6867431aa434702
Francesco Chicchiriccò <ilgrosso@apache.org>
2017-10-16 10:38:19 +0200

    [SYNCOPE-1224] Properly using pagination for bulk delete

--- client/cli/src/main/java/org/apache/syncope/client/cli/commands/user/UserSyncopeOperations.java
+++ client/cli/src/main/java/org/apache/syncope/client/cli/commands/user/UserSyncopeOperations.java
@@ -20,16 +20,20 @@ package org.apache.syncope.client.cli.commands.user;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.apache.syncope.client.cli.SyncopeServices;
 import org.apache.syncope.client.lib.SyncopeClient;
 import org.apache.syncope.common.lib.to.BulkAction;
 import org.apache.syncope.common.lib.to.BulkActionResult;
+import org.apache.syncope.common.lib.to.EntityTO;
 import org.apache.syncope.common.lib.to.PagedResult;
 import org.apache.syncope.common.lib.to.UserTO;
 import org.apache.syncope.common.rest.api.beans.AnyQuery;
 import org.apache.syncope.common.rest.api.service.UserService;
 
 public class UserSyncopeOperations {
+
+    private static final int PAGE_SIZE = 100;
 
     private final UserService userService = SyncopeServices.get(UserService.class);
 
@@ -72,23 +76,32 @@ public class UserSyncopeOperations {
 
     public Map<String, BulkActionResult.Status> deleteByAttribute(
             final String realm, final String attributeName, final String attributeValue) {
-        final List<UserTO> users = userService.search(new AnyQuery.Builder().realm(realm).
-                fiql(SyncopeClient.getUserSearchConditionBuilder().is(attributeName).equalTo(attributeValue)
-                        .query()).build()).getResult();
-        return deleteBulk(users);
+
+        return bulkDelete(new AnyQuery.Builder().realm(realm).fiql(
+                SyncopeClient.getUserSearchConditionBuilder().is(attributeName).equalTo(attributeValue).query()).
+                build());
     }
 
     public Map<String, BulkActionResult.Status> deleteAll(final String realm) {
-        return deleteBulk(userService.search(new AnyQuery.Builder().realm(realm).details(false).build()).getResult());
+        return bulkDelete(new AnyQuery.Builder().realm(realm).details(false).build());
     }
 
-    private Map<String, BulkActionResult.Status> deleteBulk(final List<UserTO> users) {
-        final BulkAction bulkAction = new BulkAction();
+    private Map<String, BulkActionResult.Status> bulkDelete(final AnyQuery query) {
+        query.setPage(0);
+        query.setSize(0);
+        int count = userService.search(query).getTotalCount();
+
+        BulkAction bulkAction = new BulkAction();
         bulkAction.setType(BulkAction.Type.DELETE);
-        users.forEach(user -> {
-            bulkAction.getTargets().add(user.getKey());
-        });
-        final BulkActionResult bulkResult = userService.bulk(bulkAction).readEntity(BulkActionResult.class);
-        return bulkResult.getResults();
+
+        query.setSize(PAGE_SIZE);
+        for (int page = 1; page <= (count / PAGE_SIZE) + 1; page++) {
+            query.setPage(page);
+
+            bulkAction.getTargets().addAll(userService.search(query).getResult().stream().
+                    map(EntityTO::getKey).collect(Collectors.toList()));
+        }
+
+        return userService.bulk(bulkAction).readEntity(BulkActionResult.class).getResults();
     }
 }
