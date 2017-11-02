f9aed7b8d2e044041e2c82a8471988d33b302aaf
Francesco Chicchiriccò <ilgrosso@apache.org>
2017-11-02 14:03:43 +0100

    Ensure the Admin Console correctly provides details to fetch ConnId object classes info

--- client/console/src/main/java/org/apache/syncope/client/console/wizards/resources/ResourceProvisionPanel.java
+++ client/console/src/main/java/org/apache/syncope/client/console/wizards/resources/ResourceProvisionPanel.java
@@ -90,7 +90,7 @@ public class ResourceProvisionPanel extends AbstractModalPanel<Serializable> {
         actionTogglePanel = new ActionLinksTogglePanel<>("toggle", pageRef);
         add(actionTogglePanel);
 
-        wizard = new ProvisionWizardBuilder(resourceTO, pageRef);
+        wizard = new ProvisionWizardBuilder(resourceTO, adminRealm, pageRef);
 
         final ListViewPanel.Builder<ResourceProvision> builder = new ListViewPanel.Builder<ResourceProvision>(
                 ResourceProvision.class, pageRef) {
