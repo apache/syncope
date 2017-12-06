7eb8c1a0a89ef10ec68c85d7ce535bf1f3aff318
Francesco Chicchiriccò <ilgrosso@apache.org>
2017-12-06 11:46:46 +0100

    Aligning custom SpringBeanJobFactory with latest Spring

--- core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/job/AutowiringSpringBeanJobFactory.java
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/job/AutowiringSpringBeanJobFactory.java
@@ -23,18 +23,27 @@ import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.PropertyAccessorFactory;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
 /**
  * An implementation of SpringBeanJobFactory that retrieves the bean from the Spring context so that autowiring and
  * transactions work.
  */
-public class SpringBeanJobFactory extends org.springframework.scheduling.quartz.SpringBeanJobFactory {
+public class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private transient AutowireCapableBeanFactory beanFactory;
 
     private String[] ignoredUnknownProperties;
 
     private SchedulerContext schedulerContext;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        beanFactory = context.getAutowireCapableBeanFactory();
+    }
 
     @Override
     public void setIgnoredUnknownProperties(final String... ignoredUnknownProperties) {
@@ -51,26 +60,24 @@ public class SpringBeanJobFactory extends org.springframework.scheduling.quartz.
 
     @Override
     protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
-        ApplicationContext ctx = ((ConfigurableApplicationContext) schedulerContext.get("applicationContext"));
-
-        Object job = ctx.getBean(bundle.getJobDetail().getKey().getName());
-        BeanWrapper wrapper = PropertyAccessorFactory.forBeanPropertyAccess(job);
-        if (isEligibleForPropertyPopulation(wrapper.getWrappedInstance())) {
-            final MutablePropertyValues pvs = new MutablePropertyValues();
+        Object job = beanFactory.getBean(bundle.getJobDetail().getKey().getName());
+        if (isEligibleForPropertyPopulation(job)) {
+            BeanWrapper bw = PropertyAccessorFactory.forBeanPropertyAccess(job);
+            MutablePropertyValues pvs = new MutablePropertyValues();
             if (this.schedulerContext != null) {
                 pvs.addPropertyValues(this.schedulerContext);
             }
             pvs.addPropertyValues(bundle.getJobDetail().getJobDataMap());
             pvs.addPropertyValues(bundle.getTrigger().getJobDataMap());
-            if (this.ignoredUnknownProperties == null) {
-                wrapper.setPropertyValues(pvs, true);
-            } else {
+            if (this.ignoredUnknownProperties != null) {
                 for (String propName : this.ignoredUnknownProperties) {
-                    if (pvs.contains(propName) && !wrapper.isWritableProperty(propName)) {
+                    if (pvs.contains(propName) && !bw.isWritableProperty(propName)) {
                         pvs.removePropertyValue(propName);
                     }
                 }
-                wrapper.setPropertyValues(pvs);
+                bw.setPropertyValues(pvs);
+            } else {
+                bw.setPropertyValues(pvs, true);
             }
         }
         return job;
