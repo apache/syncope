42c861c6eea78a493f1b903ffe3897d922960a90
Francesco Chicchiriccò <ilgrosso@apache.org>
2018-01-09 16:47:12 +0100

    Finally using the official javax.ws.rs.PATCH

--- common/rest-api/src/main/java/org/apache/syncope/common/rest/api/service/AnyObjectService.java
+++ common/rest-api/src/main/java/org/apache/syncope/common/rest/api/service/AnyObjectService.java
@@ -20,13 +20,13 @@ package org.apache.syncope.common.rest.api.service;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import org.apache.cxf.jaxrs.ext.PATCH;
 import org.apache.syncope.common.lib.patch.AnyObjectPatch;
 import org.apache.syncope.common.lib.to.AnyObjectTO;
 
