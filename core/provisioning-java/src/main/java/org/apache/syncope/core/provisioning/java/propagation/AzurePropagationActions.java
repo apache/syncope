31ba14ed13b0ec6957abb2fdf375284efcd31c65
skylark17 <matteo.alessandroni@tirasa.net>
2018-03-19 17:02:16 +0100

    [SYNCOPE-1283] Fix checkstyle

--- core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/propagation/AzurePropagationActions.java
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/propagation/AzurePropagationActions.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.syncope.core.provisioning.java.propagation;
 
 import java.util.HashSet;
@@ -56,9 +74,9 @@ public class AzurePropagationActions extends DefaultPropagationActions {
     @Autowired
     private AnyUtilsFactory anyUtilsFactory;
 
-    private final static String USER_MAIL_NICKNAME = "mailNickname";
+    private static final String USER_MAIL_NICKNAME = "mailNickname";
 
-    private final static String GROUP_MAIL_NICKNAME = "mailNickname";
+    private static final String GROUP_MAIL_NICKNAME = "mailNickname";
 
     protected String getAzureIdSchema() {
         return "AzureUserId";
