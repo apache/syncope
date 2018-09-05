382a5a05e0676191d46fd5b31fca29471e67c017
fmartelli <fabio.martelli@gmail.com>
2018-09-05 15:46:16 +0200

    [SYNCOPE-1361] provides a fix into LoggerLoader

--- core/logic/src/main/java/org/apache/syncope/core/logic/init/LoggerLoader.java
+++ core/logic/src/main/java/org/apache/syncope/core/logic/init/LoggerLoader.java
@@ -122,12 +122,13 @@ public class LoggerLoader implements SyncopeLoader {
                             AuditLoggerName.getAuditEventLoggerName(entry.getKey(), event.toLoggerName());
                     LoggerConfig eventLogConf = ctx.getConfiguration().getLoggerConfig(domainAuditLoggerName);
 
-                    if (LogManager.ROOT_LOGGER_NAME.equals(eventLogConf.getName())) {
+                    boolean isRootLogConf = LogManager.ROOT_LOGGER_NAME.equals(eventLogConf.getName());
+                    if (isRootLogConf) {
                         eventLogConf = new LoggerConfig(domainAuditLoggerName, null, false);
                     }
                     addAppenderToContext(ctx, auditAppender, eventLogConf);
                     eventLogConf.setLevel(Level.DEBUG);
-                    if (LogManager.ROOT_LOGGER_NAME.equals(eventLogConf.getName())) {
+                    if (isRootLogConf) {
                         ctx.getConfiguration().addLogger(domainAuditLoggerName, eventLogConf);
                     }
                 }
