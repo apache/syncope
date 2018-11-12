5937c0844cafab71cfcabbcd502625a59b4f512c
Andrea Patricelli <andreapatricelli@apache.org>
2018-11-12 17:30:17 +0100

    Added Active Directory pull actions

---
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/pushpull/ADMembershipPullActions.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.syncope.core.provisioning.java.pushpull;
+
+import java.util.Optional;
+import org.apache.syncope.common.lib.types.ConnConfProperty;
+import org.apache.syncope.core.provisioning.api.Connector;
+
+/**
+ * Simple action for pulling Active Directory groups memberships to Syncope group memberships, when the same resource is
+ * configured for both users and groups.
+ *
+ * @see org.apache.syncope.core.provisioning.java.propagation.LDAPMembershipPropagationActions
+ */
+public class ADMembershipPullActions extends LDAPMembershipPullActions {
+
+    /**
+     * Allows easy subclassing for the ConnId AD connector bundle.
+     *
+     * @param connector A Connector instance to query for the groupMemberAttribute property name
+     * @return the name of the attribute used to keep track of group memberships
+     */
+    @Override
+    protected String getGroupMembershipAttrName(final Connector connector) {
+        Optional<ConnConfProperty> groupMembership = connector.getConnInstance().getConf().stream().
+                filter(property -> "groupMemberReferenceAttribute".equals(property.getSchema().getName())
+                && !property.getValues().isEmpty()).
+                findFirst();
+
+        return groupMembership.isPresent()
+                ? (String) groupMembership.get().getValues().get(0)
+                : "member";
+    }
+
+}
