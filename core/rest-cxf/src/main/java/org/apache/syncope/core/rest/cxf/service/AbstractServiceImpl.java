cc52bb8086b534facf74e34f31b02a9fb295de54
Andrea Patricelli <andreapatricelli@apache.org>
2019-01-25 16:17:19 +0100

    [SYNCOPE-1428] read by key not authenticated now returns 401

--- core/rest-cxf/src/main/java/org/apache/syncope/core/rest/cxf/service/AbstractServiceImpl.java
+++ core/rest-cxf/src/main/java/org/apache/syncope/core/rest/cxf/service/AbstractServiceImpl.java
@@ -44,7 +44,6 @@ import org.apache.syncope.common.rest.api.service.JAXRSService;
 import org.apache.syncope.common.rest.api.Preference;
 import org.apache.syncope.common.rest.api.RESTHeaders;
 import org.apache.syncope.core.persistence.api.dao.AnyDAO;
-import org.apache.syncope.core.persistence.api.dao.NotFoundException;
 import org.apache.syncope.core.persistence.api.search.SearchCondVisitor;
 import org.apache.syncope.core.persistence.api.dao.search.OrderByClause;
 import org.apache.syncope.core.persistence.api.dao.search.SearchCond;
@@ -70,9 +69,6 @@ abstract class AbstractServiceImpl implements JAXRSService {
         String actualKey = pretendingKey;
         if (!SyncopeConstants.UUID_PATTERN.matcher(pretendingKey).matches()) {
             actualKey = dao.findKey(pretendingKey);
-            if (actualKey == null) {
-                throw new NotFoundException("User, Group or Any Object for " + pretendingKey);
-            }
         }
 
         return actualKey;
