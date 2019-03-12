b723ee53ebeeabd4e510ff7593b87cf167e5aaf9
Francesco Chicchiriccò <ilgrosso@apache.org>
2019-03-12 14:09:02 +0100

    [SYNCOPE-1450] Extending the masking filter for audit entries

--- core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/AuditEntry.java
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/AuditEntry.java
@@ -23,12 +23,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.SerializationUtils;
+import org.apache.syncope.common.lib.request.UserCR;
+import org.apache.syncope.common.lib.request.UserUR;
 import org.apache.syncope.common.lib.to.UserTO;
 import org.apache.syncope.common.lib.types.AuditLoggerName;
 
 public class AuditEntry implements Serializable {
 
     private static final long serialVersionUID = -2299082316063743582L;
+
+    private static final String MASKED_VALUE = "<MASKED>";
 
     private final String who;
 
@@ -52,28 +56,43 @@ public class AuditEntry implements Serializable {
 
         this.who = who;
         this.logger = logger;
-        this.before = filterUserPassword(before);
-        this.output = filterUserPassword(output);
+        this.before = maskSensitive(before);
+        this.output = maskSensitive(output);
         this.input = ArrayUtils.clone(input);
         if (this.input != null) {
             for (int i = 0; i < this.input.length; i++) {
-                this.input[i] = filterUserPassword(this.input[i]);
+                this.input[i] = maskSensitive(this.input[i]);
             }
         }
     }
 
-    private Object filterUserPassword(final Object object) {
-        Object filtered;
+    private Object maskSensitive(final Object object) {
+        Object masked;
 
         if (object instanceof UserTO) {
-            UserTO user = SerializationUtils.clone((UserTO) object);
-            user.setPassword(null);
-            filtered = user;
+            masked = SerializationUtils.clone((UserTO) object);
+            if (((UserTO) masked).getPassword() != null) {
+                ((UserTO) masked).setPassword(MASKED_VALUE);
+            }
+            if (((UserTO) masked).getSecurityAnswer() != null) {
+                ((UserTO) masked).setSecurityAnswer(MASKED_VALUE);
+            }
+        } else if (object instanceof UserCR) {
+            masked = SerializationUtils.clone((UserCR) object);
+            if (((UserCR) masked).getPassword() != null) {
+                ((UserCR) masked).setPassword(MASKED_VALUE);
+            }
+            if (((UserCR) masked).getSecurityAnswer() != null) {
+                ((UserCR) masked).setSecurityAnswer(MASKED_VALUE);
+            }
+        } else if (object instanceof UserUR && ((UserUR) object).getPassword() != null) {
+            masked = SerializationUtils.clone((UserUR) object);
+            ((UserUR) masked).getPassword().setValue(MASKED_VALUE);
         } else {
-            filtered = object;
+            masked = object;
         }
 
-        return filtered;
+        return masked;
     }
 
     public String getWho() {
@@ -95,5 +114,4 @@ public class AuditEntry implements Serializable {
     public Object[] getInput() {
         return input;
     }
-
 }
