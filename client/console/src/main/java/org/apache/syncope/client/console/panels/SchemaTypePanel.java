88c6bd6af9685903648f2040b7d9312a24d39b76
Andrea Patricelli <andreapatricelli@apache.org>
2019-06-04 11:11:05 +0200

    removed unnecessary if statement

--- client/console/src/main/java/org/apache/syncope/client/console/panels/SchemaTypePanel.java
+++ client/console/src/main/java/org/apache/syncope/client/console/panels/SchemaTypePanel.java
@@ -304,13 +304,11 @@ public class SchemaTypePanel extends TypesDirectoryPanel<SchemaTO, SchemaProvide
             AjaxRequestTarget target = payload.getTarget();
 
             keyword = payload.getKeyword();
-            if (keyword != null) {
-                if (!keyword.startsWith("*")) {
-                    keyword = "*" + keyword;
-                }
-                if (!keyword.endsWith("*")) {
-                    keyword = keyword + "*";
-                }
+            if (!StringUtils.startsWith(keyword, "*")) {
+                keyword = "*" + keyword;
+            }
+            if (!StringUtils.endsWith(keyword, "*")) {
+                keyword = keyword + "*";
             }
 
             updateResultTable(target);
