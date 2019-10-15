98064a5d9fbcbf49f8ca269483a8fde5acc9492d
Marco Di Sabatino Di Diodoro <marco.disabatino@tirasa.net>
2019-10-15 14:28:43 +0200

    [SYNCOPE-1504]

--- core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/pushpull/PullJobDelegate.java
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/pushpull/PullJobDelegate.java
@@ -356,7 +356,8 @@ public class PullJobDelegate extends AbstractProvisioningJobDelegate<PullTask> i
                         AnyUtils anyUtils = anyUtilsFactory.getInstance(provision.getAnyType().getKind());
                         profile.getResults().stream().
                                 filter(result -> result.getUidValue() != null
-                                && result.getOperation() == ResourceOperation.CREATE).
+                                && result.getOperation() == ResourceOperation.CREATE
+                                && result.getAnyType().equals(provision.getAnyType().getKey())).
                                 forEach(result -> {
                                     anyUtils.addAttr(result.getKey(), provision.getUidOnCreate(), result.getUidValue());
                                 });
