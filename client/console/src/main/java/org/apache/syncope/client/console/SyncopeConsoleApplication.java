9f6d97a6847db08945a111d8a712fc13b9863d55
Francesco Chicchiriccò <ilgrosso@apache.org>
2019-11-20 15:08:03 +0100

    [SYNCOPE-1513] Restoring console IT tests

--- client/console/src/main/java/org/apache/syncope/client/console/SyncopeConsoleApplication.java
+++ client/console/src/main/java/org/apache/syncope/client/console/SyncopeConsoleApplication.java
@@ -77,9 +77,8 @@ public class SyncopeConsoleApplication extends AuthenticatedWebApplication {
     private static final String CONSOLE_PROPERTIES = "console.properties";
 
     public static final List<Locale> SUPPORTED_LOCALES = Collections.unmodifiableList(Arrays.asList(
-            new Locale[] {
-                Locale.ENGLISH, Locale.ITALIAN, new Locale("pt", "BR"), new Locale("ru"), Locale.JAPANESE
-            }));
+            Locale.ENGLISH, Locale.ITALIAN, new Locale("pt", "BR"), new Locale("ru"), Locale.JAPANESE
+    ));
 
     public static SyncopeConsoleApplication get() {
         return (SyncopeConsoleApplication) WebApplication.get();
