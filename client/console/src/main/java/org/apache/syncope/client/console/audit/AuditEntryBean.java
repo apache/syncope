d34ca5210ab64da2e4e305b32e1d3181f9fb1944
Misagh Moayyed <mm1844@gmail.com>
2019-11-28 16:16:32 +0400

    clean up formatting

--- client/console/src/main/java/org/apache/syncope/client/console/audit/AuditEntryBean.java
+++ client/console/src/main/java/org/apache/syncope/client/console/audit/AuditEntryBean.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.syncope.client.console.audit;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -126,6 +144,15 @@ public class AuditEntryBean implements Serializable {
     public int hashCode() {
         return new HashCodeBuilder().
             append(key).
+            append(loggerName).
+            append(inputs).
+            append(who).
+            append(subCategory).
+            append(event).
+            append(result).
+            append(before).
+            append(output).
+            append(date).
             build();
     }
 
@@ -143,6 +170,15 @@ public class AuditEntryBean implements Serializable {
         final AuditEntryBean other = (AuditEntryBean) obj;
         return new EqualsBuilder().
             append(key, other.key).
+            append(loggerName, other.loggerName).
+            append(inputs, other.inputs).
+            append(who, other.who).
+            append(subCategory, other.subCategory).
+            append(event, other.event).
+            append(result, other.result).
+            append(before, other.before).
+            append(output, other.output).
+            append(date, other.date).
             build();
     }
 
@@ -150,6 +186,15 @@ public class AuditEntryBean implements Serializable {
     public String toString() {
         return new ToStringBuilder(this).
             append(key).
+            append(loggerName).
+            append(inputs).
+            append(who).
+            append(subCategory).
+            append(event).
+            append(result).
+            append(before).
+            append(output).
+            append(date).
             build();
     }
 }
