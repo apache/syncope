9da046922de60acc1af996f07e1c940e061a599d
Misagh Moayyed <mm1844@gmail.com>
2019-12-05 11:01:06 +0400

    add additional audit tests for read/search ops

--- fit/core-reference/src/test/java/org/apache/syncope/fit/core/AuditITCase.java
+++ fit/core-reference/src/test/java/org/apache/syncope/fit/core/AuditITCase.java
@@ -18,26 +18,28 @@
  */
 package org.apache.syncope.fit.core;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.syncope.common.lib.SyncopeConstants;
 import org.apache.syncope.common.lib.to.AnyObjectTO;
 import org.apache.syncope.common.lib.to.AuditEntryTO;
 import org.apache.syncope.common.lib.to.GroupTO;
+import org.apache.syncope.common.lib.to.PagedResult;
 import org.apache.syncope.common.lib.to.UserTO;
 import org.apache.syncope.common.lib.types.AuditElements;
+import org.apache.syncope.common.rest.api.beans.AnyQuery;
 import org.apache.syncope.common.rest.api.beans.AuditQuery;
 import org.apache.syncope.core.persistence.api.entity.anyobject.AnyObject;
 import org.apache.syncope.fit.AbstractITCase;
 import org.junit.jupiter.api.Test;
 
-public class AuditITCase extends AbstractITCase {
+import static org.junit.jupiter.api.Assertions.*;
 
-    private AuditEntryTO query(final AuditQuery query, final int maxWaitSeconds) {
+public class AuditITCase extends AbstractITCase {
+    private static final int MAX_WAIT_SECONDS = 30;
+
+    private static AuditEntryTO query(final AuditQuery query, final int maxWaitSeconds, final boolean failIfEmpty) {
         int i = 0;
         List<AuditEntryTO> results = Collections.emptyList();
         do {
@@ -51,10 +53,30 @@ public class AuditITCase extends AbstractITCase {
             i++;
         } while (results.isEmpty() && i < maxWaitSeconds);
         if (results.isEmpty()) {
-            fail("Timeout when executing query for key " + query.getEntityKey());
+            if (failIfEmpty) {
+                fail("Timeout when executing query for key " + query.getEntityKey());
+            }
+            return null;
         }
 
         return results.get(0);
+    }
+
+    @Test
+    public void userReadAndSearchYieldsNoAudit() {
+        PagedResult<UserTO> users = userService.search(
+            new AnyQuery.Builder().realm(SyncopeConstants.ROOT_REALM).page(1).size(2).build());
+        assertNotNull(users);
+        assertFalse(users.getResult().isEmpty());
+       
+        for (UserTO user : users.getResult()) {
+            assertNotNull(userService.read(user.getKey()));
+        }
+        for (UserTO user : users.getResult()) {
+            AuditQuery query = new AuditQuery.Builder(user.getKey()).build();
+            AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, false);
+            assertNull(entry);
+        }
     }
 
     @Test
@@ -64,7 +86,7 @@ public class AuditITCase extends AbstractITCase {
 
         AuditQuery query = new AuditQuery.Builder(userTO.getKey()).orderBy("event_date desc").
                 page(1).size(1).build();
-        AuditEntryTO entry = query(query, 50);
+        AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, true);
         assertEquals(userTO.getKey(), entry.getKey());
         userService.delete(userTO.getKey());
     }
@@ -83,7 +105,7 @@ public class AuditITCase extends AbstractITCase {
                 event("create").
                 result(AuditElements.Result.SUCCESS).
                 build();
-        AuditEntryTO entry = query(query, 50);
+        AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, true);
         assertEquals(userTO.getKey(), entry.getKey());
         userService.delete(userTO.getKey());
     }
@@ -95,9 +117,25 @@ public class AuditITCase extends AbstractITCase {
 
         AuditQuery query = new AuditQuery.Builder(groupTO.getKey()).orderBy("event_date desc").
                 page(1).size(1).build();
-        AuditEntryTO entry = query(query, 50);
+        AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, true);
         assertEquals(groupTO.getKey(), entry.getKey());
         groupService.delete(groupTO.getKey());
+    }
+
+    @Test
+    public void groupReadAndSearchYieldsNoAudit() {
+        PagedResult<GroupTO> groups = groupService.search(
+            new AnyQuery.Builder().realm(SyncopeConstants.ROOT_REALM).build());
+        assertNotNull(groups);
+        assertFalse(groups.getResult().isEmpty());
+        for (GroupTO groupTO : groups.getResult()) {
+            assertNotNull(groupService.read(groupTO.getKey()));
+        }
+        for (GroupTO groupTO : groups.getResult()) {
+            AuditQuery query = new AuditQuery.Builder(groupTO.getKey()).build();
+            AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, false);
+            assertNull(entry);
+        }
     }
 
     @Test
@@ -106,8 +144,24 @@ public class AuditITCase extends AbstractITCase {
         assertNotNull(anyObjectTO.getKey());
         AuditQuery query = new AuditQuery.Builder(anyObjectTO.getKey()).orderBy("event_date desc").
             page(1).size(1).build();
-        AuditEntryTO entry = query(query, 50);
+        AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, true);
         assertEquals(anyObjectTO.getKey(), entry.getKey());
         anyObjectService.delete(anyObjectTO.getKey());
+    }
+
+    @Test
+    public void anyObjectReadAndSearchYieldsNoAudit() {
+        PagedResult<AnyObjectTO> anyObjects = anyObjectService.search(
+            new AnyQuery.Builder().realm(SyncopeConstants.ROOT_REALM).build());
+        assertNotNull(anyObjects);
+        assertFalse(anyObjects.getResult().isEmpty());
+        for (AnyObjectTO anyObjectTO : anyObjects.getResult()) {
+            assertNotNull(anyObjectService.read(anyObjectTO.getKey()));
+        }
+        for (AnyObjectTO anyObjectTO : anyObjects.getResult()) {
+            AuditQuery query = new AuditQuery.Builder(anyObjectTO.getKey()).build();
+            AuditEntryTO entry = query(query, MAX_WAIT_SECONDS, false);
+            assertNull(entry);
+        }
     }
 }
