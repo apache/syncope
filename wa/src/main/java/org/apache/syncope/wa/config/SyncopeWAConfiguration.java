80b941e0ace052034995b86315b95bdb482923c8
Francesco Chicchiriccò <ilgrosso@apache.org>
2020-03-16 14:05:06 +0100

    [SYNCOPE-1545] Using CAS BOM

--- wa/src/main/java/org/apache/syncope/wa/config/SyncopeWAConfiguration.java
+++ wa/src/main/java/org/apache/syncope/wa/config/SyncopeWAConfiguration.java
@@ -6,7 +6,8 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -14,26 +15,26 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
-
 package org.apache.syncope.wa.config;
 
+import java.io.Serializable;
 import org.apereo.cas.services.DefaultRegisteredServiceEntityMapper;
 import org.apereo.cas.services.RegisteredServiceEntityMapper;
-
 import org.apache.syncope.common.keymaster.client.api.model.NetworkService;
 import org.apache.syncope.common.keymaster.client.api.startstop.KeymasterStart;
 import org.apache.syncope.common.keymaster.client.api.startstop.KeymasterStop;
+import org.apereo.cas.services.RegisteredService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 public class SyncopeWAConfiguration {
+
     @Bean
     @ConditionalOnProperty(name = "cas.serviceRegistry.rest.url")
-    public RegisteredServiceEntityMapper registeredServiceEntityMapper() {
+    public RegisteredServiceEntityMapper<RegisteredService, Serializable> registeredServiceEntityMapper() {
         return new DefaultRegisteredServiceEntityMapper();
     }
 
