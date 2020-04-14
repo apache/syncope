931d9716505fb8fc05fd1173f12072ef435f9983
Misagh Moayyed <mm1844@gmail.com>
2020-04-14 17:59:59 +0430

    SYNCOPE-1553: map auth module conf to wa properties

--- wa/bootstrap/src/main/java/org/apache/syncope/wa/bootstrap/SyncopeWAPropertySourceLocator.java
+++ wa/bootstrap/src/main/java/org/apache/syncope/wa/bootstrap/SyncopeWAPropertySourceLocator.java
@@ -19,6 +19,17 @@
 
 package org.apache.syncope.wa.bootstrap;
 
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.configuration.CasCoreConfigurationUtils;
+import org.apereo.cas.configuration.model.support.generic.AcceptAuthenticationProperties;
+import org.apereo.cas.configuration.model.support.ldap.LdapAuthenticationProperties;
+import org.apereo.cas.configuration.model.support.syncope.SyncopeAuthenticationProperties;
+
+import org.apache.syncope.common.lib.auth.AuthModuleConf;
+import org.apache.syncope.common.lib.auth.LDAPAuthModuleConf;
+import org.apache.syncope.common.lib.auth.StaticAuthModuleConf;
+import org.apache.syncope.common.lib.auth.SyncopeAuthModuleConf;
+import org.apache.syncope.common.rest.api.service.AuthModuleService;
 import org.apache.syncope.wa.WARestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,8 +39,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Order
 public class SyncopeWAPropertySourceLocator implements PropertySourceLocator {
@@ -41,19 +52,70 @@ public class SyncopeWAPropertySourceLocator implements PropertySourceLocator {
         this.waRestClient = waRestClient;
     }
 
+    private static void mapSyncopeAuthModuleConf(final CasConfigurationProperties casProperties,
+                                                 final AuthModuleConf authConf) {
+        SyncopeAuthModuleConf conf = SyncopeAuthModuleConf.class.cast(authConf);
+        SyncopeAuthenticationProperties syncopeProps = new SyncopeAuthenticationProperties();
+        syncopeProps.setName(conf.getName());
+        syncopeProps.setDomain(conf.getDomain());
+        syncopeProps.setUrl(conf.getUrl());
+        casProperties.getAuthn().setSyncope(syncopeProps);
+    }
+
+    private static void mapStaticAuthModuleConf(final CasConfigurationProperties casProperties,
+                                                final AuthModuleConf authConf) {
+        StaticAuthModuleConf conf = StaticAuthModuleConf.class.cast(authConf);
+        AcceptAuthenticationProperties staticProps = new AcceptAuthenticationProperties();
+        staticProps.setName(conf.getName());
+        String users = conf.getUsers().entrySet().stream().
+            map(entry -> entry.getKey() + "::" + entry.getValue()).
+            collect(Collectors.joining(","));
+        staticProps.setUsers(users);
+        casProperties.getAuthn().setAccept(staticProps);
+    }
+
+    private static void mapLdapAuthModuleConf(final CasConfigurationProperties casProperties,
+                                              final AuthModuleConf authConf) {
+        LDAPAuthModuleConf ldapConf = LDAPAuthModuleConf.class.cast(authConf);
+
+        LdapAuthenticationProperties ldapProps = new LdapAuthenticationProperties();
+        ldapProps.setName(ldapConf.getName());
+        ldapProps.setBaseDn(ldapConf.getBaseDn());
+        ldapProps.setBindCredential(ldapConf.getBindCredential());
+        ldapProps.setSearchFilter(ldapConf.getSearchFilter());
+        ldapProps.setPrincipalAttributeId(ldapConf.getUserIdAttribute());
+        ldapProps.setLdapUrl(ldapConf.getLdapUrl());
+        ldapProps.setSubtreeSearch(ldapConf.isSubtreeSearch());
+
+        casProperties.getAuthn().getLdap().add(ldapProps);
+    }
+
     @Override
     public PropertySource<?> locate(final Environment environment) {
-        try {
-            Map<String, Object> properties = new HashMap<>();
-            if (WARestClient.isReady()) {
-                LOG.info("Bootstrapping WA configuration");
-                return new MapPropertySource(getClass().getName(), properties);
-            }
-
+        if (!WARestClient.isReady()) {
             LOG.warn("Application context is not ready to bootstrap WA configuration");
             return null;
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Unable to fetch settings", e);
         }
+        LOG.info("Bootstrapping WA configuration");
+        CasConfigurationProperties casProperties = new CasConfigurationProperties();
+        AuthModuleService authService = waRestClient.getSyncopeClient().getService(AuthModuleService.class);
+        authService.list().forEach(authModuleTO -> {
+
+            AuthModuleConf authConf = authModuleTO.getConf();
+            LOG.debug("Mapping auth module {}:{} as conf {}", authModuleTO.getKey(),
+                authModuleTO.getName(), authConf.getName());
+            if (authConf instanceof LDAPAuthModuleConf) {
+                mapLdapAuthModuleConf(casProperties, authConf);
+            }
+            if (authConf instanceof StaticAuthModuleConf) {
+                mapStaticAuthModuleConf(casProperties, authConf);
+            }
+            if (authConf instanceof SyncopeAuthModuleConf) {
+                mapSyncopeAuthModuleConf(casProperties, authConf);
+            }
+        });
+        Map<String, Object> properties = CasCoreConfigurationUtils.asMap(casProperties.withHolder());
+        LOG.debug("Collected WA properties: {}", properties);
+        return new MapPropertySource(getClass().getName(), properties);
     }
 }
