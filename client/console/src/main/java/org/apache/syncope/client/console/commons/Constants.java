fc8845e4b878d86d2cdca81456492980e081e84e
Francesco Chicchiriccò <ilgrosso@apache.org>
2020-06-11 16:26:51 +0200

    Fixing entitlements for Reports

--- client/console/src/main/java/org/apache/syncope/client/console/commons/Constants.java
+++ client/console/src/main/java/org/apache/syncope/client/console/commons/Constants.java
@@ -133,6 +133,8 @@ public final class Constants {
 
     public static final String PREF_MAIL_TEMPLATE_PAGINATOR_ROWS = "mail.template.paginator.rows";
 
+    public static final String PREF_REPORT_TEMPLATE_PAGINATOR_ROWS = "report.template.paginator.rows";
+
     public static final String PREF_PROPAGATION_TASKS_PAGINATOR_ROWS = "proagationtasks.paginator.rows";
 
     public static final String PREF_CONNECTOR_HISTORY_CONF_PAGINATOR_ROWS = "connectorhistoryconf.paginator.rows";
