b882434479de5c42c10114d9e8d9ef11de6f7c34
Francesco Chicchiriccò <ilgrosso@apache.org>
2020-10-21 13:34:05 +0200

    Cleanup

--- ext/flowable/client-console/src/main/java/org/apache/syncope/client/console/rest/UserRequestRestClient.java
+++ ext/flowable/client-console/src/main/java/org/apache/syncope/client/console/rest/UserRequestRestClient.java
@@ -18,9 +18,6 @@
  */
 package org.apache.syncope.client.console.rest;
 
-import static org.apache.syncope.client.console.rest.BaseRestClient.LOG;
-import static org.apache.syncope.client.console.rest.BaseRestClient.getStatus;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
