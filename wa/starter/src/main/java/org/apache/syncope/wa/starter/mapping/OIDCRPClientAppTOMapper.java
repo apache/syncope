19255512ce9fe9c51ee659b7821e3ae86d416eda
Misagh <mm1844@gmail.com>
2021-06-17 12:17:12 +0400

    fix tests - enable remote debugging for SRA on tests

--- wa/starter/src/main/java/org/apache/syncope/wa/starter/mapping/OIDCRPClientAppTOMapper.java
+++ wa/starter/src/main/java/org/apache/syncope/wa/starter/mapping/OIDCRPClientAppTOMapper.java
@@ -64,7 +64,7 @@ public class OIDCRPClientAppTOMapper extends AbstractClientAppMapper {
         OidcRegisteredService service = new OidcRegisteredService();
         setCommon(service, rp);
 
-        service.setServiceId(Stream.concat(rp.getRedirectUris().stream(), Stream.of(rp.getLogoutUri())).
+        service.setServiceId(rp.getRedirectUris().stream().
                 filter(Objects::nonNull).
                 collect(Collectors.joining("|")));
         service.setClientId(rp.getClientId());
@@ -73,6 +73,7 @@ public class OIDCRPClientAppTOMapper extends AbstractClientAppMapper {
         if (!service.isSignIdToken()) {
             service.setIdTokenSigningAlg("none");
         }
+        service.setJwtAccessToken(true);
         service.setSupportedGrantTypes(rp.getSupportedGrantTypes().stream().
                 map(OIDCGrantType::name).collect(Collectors.toCollection(HashSet::new)));
         service.setSupportedResponseTypes(rp.getSupportedResponseTypes().stream().
