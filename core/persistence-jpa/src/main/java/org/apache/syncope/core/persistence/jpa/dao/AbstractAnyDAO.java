1d66d86f2e963799ebde5b099578543e2d02e085
Andrea Patricelli <andreapatricelli@apache.org>
2021-07-01 16:22:00 +0200

    adding parenthesis to perform the correct query while searching by plainAttrValue

--- core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/AbstractAnyDAO.java
+++ core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/AbstractAnyDAO.java
@@ -189,7 +189,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
 
     private Query findByPlainAttrValueQuery(final String entityName, final boolean ignoreCaseMatch) {
         String query = "SELECT e FROM " + entityName + " e"
-                + " WHERE e.attribute.schema.id = :schemaKey AND (e.stringValue IS NOT NULL"
+                + " WHERE e.attribute.schema.id = :schemaKey AND ((e.stringValue IS NOT NULL"
                 + " AND "
                 + (ignoreCaseMatch ? "LOWER(" : "") + "e.stringValue" + (ignoreCaseMatch ? ")" : "")
                 + " = "
@@ -197,7 +197,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
                 + " OR (e.booleanValue IS NOT NULL AND e.booleanValue = :booleanValue)"
                 + " OR (e.dateValue IS NOT NULL AND e.dateValue = :dateValue)"
                 + " OR (e.longValue IS NOT NULL AND e.longValue = :longValue)"
-                + " OR (e.doubleValue IS NOT NULL AND e.doubleValue = :doubleValue)";
+                + " OR (e.doubleValue IS NOT NULL AND e.doubleValue = :doubleValue))";
         return entityManager().createQuery(query);
     }
 
