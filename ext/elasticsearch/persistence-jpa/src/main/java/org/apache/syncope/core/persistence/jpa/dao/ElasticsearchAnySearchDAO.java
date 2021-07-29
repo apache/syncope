6d7e8f62593fffb5b2b5795d5ec55801ad8a4d84
Francesco Chicchiriccò <ilgrosso@apache.org>
2021-07-29 17:15:12 +0200

    Escape anpersand when searching in Elasticsearch

--- ext/elasticsearch/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/ElasticsearchAnySearchDAO.java
+++ ext/elasticsearch/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/ElasticsearchAnySearchDAO.java
@@ -85,7 +85,7 @@ public class ElasticsearchAnySearchDAO extends AbstractAnySearchDAO {
     protected static final QueryBuilder MATCH_ALL_QUERY_BUILDER = new MatchAllQueryBuilder();
 
     protected static final char[] ELASTICSEARCH_REGEX_CHARS = new char[] {
-        '.', '?', '+', '*', '|', '{', '}', '[', ']', '(', ')', '"', '\\' };
+        '.', '?', '+', '*', '|', '{', '}', '[', ']', '(', ')', '"', '\\', '&' };
 
     protected static String escapeForLikeRegex(final char c) {
         StringBuilder output = new StringBuilder();
