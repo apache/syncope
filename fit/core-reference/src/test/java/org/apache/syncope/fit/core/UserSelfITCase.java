75b892482fb58828ffcd146209ba3c2b2373f9b3
Francesco Chicchiriccò <ilgrosso@apache.org>
2022-02-09 16:10:26 +0100

    Upgrading slf4j

--- fit/core-reference/src/test/java/org/apache/syncope/fit/core/UserSelfITCase.java
+++ fit/core-reference/src/test/java/org/apache/syncope/fit/core/UserSelfITCase.java
@@ -18,6 +18,7 @@
  */
 package org.apache.syncope.fit.core;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -31,6 +32,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
@@ -347,8 +349,14 @@ public class UserSelfITCase extends AbstractITCase {
                 // ignore
             }
         }
-        PagedResult<UserTO> tokenized = userService.search(new AnyQuery.Builder().fiql("token!=$null").build());
-        assertTrue(tokenized.getResult().stream().anyMatch(u -> user.getUsername().equals(u.getUsername())));
+        await().atMost(MAX_WAIT_SECONDS, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(() -> {
+            try {
+                PagedResult<UserTO> tokenized = userService.search(new AnyQuery.Builder().fiql("token!=$null").build());
+                return tokenized.getResult().stream().anyMatch(u -> user.getUsername().equals(u.getUsername()));
+            } catch (Exception e) {
+                return false;
+            }
+        });
 
         // 4. get token (normally sent via e-mail, now reading as admin)
         String token = userService.read(read.getKey()).getToken();
