apache/syncope

commit d2524ead7102e38f6b490c3da792f43bcd81ae51

[SYNCOPE-1392] Removing BeanUtils#copyProperties

core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/data/SchemaDataBinderImpl.java
@@ -33,7 +33,6 @@ import org.apache.syncope.core.persistence.api.dao.PlainSchemaDAO;
 import org.apache.syncope.core.persistence.api.entity.DerSchema;
 import org.apache.syncope.core.persistence.api.entity.PlainSchema;
 import org.apache.syncope.core.persistence.api.entity.VirSchema;
-import org.apache.syncope.core.spring.BeanUtils;
 import org.apache.syncope.core.provisioning.java.jexl.JexlUtils;
 import org.apache.syncope.core.persistence.api.dao.AnyTypeClassDAO;
 import org.apache.syncope.core.persistence.api.dao.AnyTypeDAO;
@@ -61,8 +60,6 @@ import org.springframework.stereotype.Component;
 public class SchemaDataBinderImpl implements SchemaDataBinder {
 
     private static final Logger LOG = LoggerFactory.getLogger(SchemaDataBinder.class);
-
-    private static final String[] IGNORE_PROPERTIES = { "anyTypeClass", "provision", "resource", "validator" };
 
     @Autowired
     private AnyTypeClassDAO anyTypeClassDAO;
@@ -119,7 +116,19 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
             throw sce;
         }
 
-        BeanUtils.copyProperties(schemaTO, schema, IGNORE_PROPERTIES);
+        schema.setKey(schemaTO.getKey());
+        schema.setType(schemaTO.getType());
+        schema.setCipherAlgorithm(schemaTO.getCipherAlgorithm());
+        schema.setConversionPattern(schemaTO.getConversionPattern());
+        schema.setEnumerationKeys(schemaTO.getEnumerationKeys());
+        schema.setEnumerationValues(schemaTO.getEnumerationValues());
+        schema.setMandatoryCondition(schemaTO.getMandatoryCondition());
+        schema.setMimeType(schemaTO.getMimeType());
+        schema.setMultivalue(schemaTO.isMultivalue());
+        schema.setReadonly(schemaTO.isReadonly());
+        schema.setSecretKey(schemaTO.getSecretKey());
+        schema.setUniqueConstraint(schemaTO.isUniqueConstraint());
+
         labels(schemaTO, schema);
 
         if (schemaTO.getValidator() == null) {
@@ -201,8 +210,19 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
         }
 
         PlainSchemaTO schemaTO = new PlainSchemaTO();
+        schemaTO.setKey(schema.getKey());
+        schemaTO.setType(schema.getType());
+        schemaTO.setCipherAlgorithm(schema.getCipherAlgorithm());
+        schemaTO.setConversionPattern(schema.getConversionPattern());
+        schemaTO.setEnumerationKeys(schema.getEnumerationKeys());
+        schemaTO.setEnumerationValues(schema.getEnumerationValues());
+        schemaTO.setMandatoryCondition(schema.getMandatoryCondition());
+        schemaTO.setMimeType(schema.getMimeType());
+        schemaTO.setMultivalue(schema.isMultivalue());
+        schemaTO.setReadonly(schema.isReadonly());
+        schemaTO.setSecretKey(schema.getSecretKey());
+        schemaTO.setUniqueConstraint(schema.isUniqueConstraint());
 
-        BeanUtils.copyProperties(schema, schemaTO, IGNORE_PROPERTIES);
         labels(schema, schemaTO);
 
         schemaTO.setAnyTypeClass(schema.getAnyTypeClass() == null ? null : schema.getAnyTypeClass().getKey());
@@ -234,7 +254,9 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
             throw scce;
         }
 
-        BeanUtils.copyProperties(schemaTO, schema, IGNORE_PROPERTIES);
+        schema.setKey(schemaTO.getKey());
+        schema.setExpression(schemaTO.getExpression());
+
         labels(schemaTO, schema);
 
         DerSchema merged = derSchemaDAO.save(schema);
@@ -277,8 +299,9 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
         }
 
         DerSchemaTO schemaTO = new DerSchemaTO();
+        schemaTO.setKey(schema.getKey());
+        schemaTO.setExpression(schema.getExpression());
 
-        BeanUtils.copyProperties(schema, schemaTO, IGNORE_PROPERTIES);
         labels(schema, schemaTO);
 
         schemaTO.setAnyTypeClass(schema.getAnyTypeClass() == null ? null : schema.getAnyTypeClass().getKey());
@@ -288,7 +311,10 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
 
     // --------------- VIRTUAL -----------------
     private VirSchema fill(final VirSchema schema, final VirSchemaTO schemaTO) {
-        BeanUtils.copyProperties(schemaTO, schema, IGNORE_PROPERTIES);
+        schema.setKey(schemaTO.getKey());
+        schema.setExtAttrName(schemaTO.getExtAttrName());
+        schema.setReadonly(schema.isReadonly());
+
         labels(schemaTO, schema);
 
         if (schemaTO.getAnyTypeClass() != null
@@ -350,8 +376,10 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
         }
 
         VirSchemaTO schemaTO = new VirSchemaTO();
+        schemaTO.setKey(schema.getKey());
+        schemaTO.setExtAttrName(schema.getExtAttrName());
+        schemaTO.setReadonly(schema.isReadonly());
 
-        BeanUtils.copyProperties(schema, schemaTO, IGNORE_PROPERTIES);
         labels(schema, schemaTO);
 
         schemaTO.setAnyTypeClass(schema.getAnyTypeClass() == null ? null : schema.getAnyTypeClass().getKey());

core/logic/src/main/java/org/apache/syncope/core/logic/LoggerLogic.java
@@ -55,7 +55,6 @@ import org.apache.syncope.core.persistence.api.dao.LoggerDAO;
 import org.apache.syncope.core.persistence.api.dao.NotFoundException;
 import org.apache.syncope.core.persistence.api.entity.EntityFactory;
 import org.apache.syncope.core.persistence.api.entity.Logger;
-import org.apache.syncope.core.spring.BeanUtils;
 import org.apache.syncope.core.provisioning.java.pushpull.PushJobDelegate;
 import org.apache.syncope.core.provisioning.java.pushpull.PullJobDelegate;
 import org.apache.syncope.core.spring.security.AuthContextUtils;
@@ -116,7 +115,8 @@ public class LoggerLogic extends AbstractTransactionalLogic<LoggerTO> {
     private List<LoggerTO> list(final LoggerType type) {
         return loggerDAO.findAll(type).stream().map(logger -> {
             LoggerTO loggerTO = new LoggerTO();
-            BeanUtils.copyProperties(logger, loggerTO);
+            loggerTO.setKey(logger.getKey());
+            loggerTO.setLevel(logger.getLevel());
             return loggerTO;
         }).collect(Collectors.toList());
     }
@@ -230,7 +230,8 @@ public class LoggerLogic extends AbstractTransactionalLogic<LoggerTO> {
         ctx.updateLoggers();
 
         LoggerTO result = new LoggerTO();
-        BeanUtils.copyProperties(syncopeLogger, result);
+        result.setKey(syncopeLogger.getKey());
+        result.setLevel(syncopeLogger.getLevel());
 
         return result;
     }
@@ -262,7 +263,8 @@ public class LoggerLogic extends AbstractTransactionalLogic<LoggerTO> {
         }
 
         LoggerTO loggerToDelete = new LoggerTO();
-        BeanUtils.copyProperties(syncopeLogger, loggerToDelete);
+        loggerToDelete.setKey(syncopeLogger.getKey());
+        loggerToDelete.setLevel(syncopeLogger.getLevel());
 
         // remove SyncopeLogger from local storage, so that LoggerLoader won't load this next time
         loggerDAO.delete(syncopeLogger);

core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/AbstractAnyDAO.java
@@ -58,7 +58,6 @@ import org.apache.syncope.core.persistence.api.entity.group.Group;
 import org.apache.syncope.core.persistence.api.entity.resource.ExternalResource;
 import org.apache.syncope.core.persistence.api.entity.user.User;
 import org.apache.syncope.core.persistence.jpa.entity.user.JPAUser;
-import org.apache.syncope.core.spring.ApplicationContextProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.transaction.annotation.Propagation;
@@ -69,40 +68,16 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
     @Autowired
     protected ApplicationEventPublisher publisher;
 
+    @Autowired
     private PlainSchemaDAO plainSchemaDAO;
 
+    @Autowired
     private DerSchemaDAO derSchemaDAO;
 
-    private DynRealmDAO dynRealmDAO;
+    @Autowired
+    protected DynRealmDAO dynRealmDAO;
 
     private AnyUtils anyUtils;
-
-    private PlainSchemaDAO plainSchemaDAO() {
-        synchronized (this) {
-            if (plainSchemaDAO == null) {
-                plainSchemaDAO = ApplicationContextProvider.getApplicationContext().getBean(PlainSchemaDAO.class);
-            }
-        }
-        return plainSchemaDAO;
-    }
-
-    private DerSchemaDAO derSchemaDAO() {
-        synchronized (this) {
-            if (derSchemaDAO == null) {
-                derSchemaDAO = ApplicationContextProvider.getApplicationContext().getBean(DerSchemaDAO.class);
-            }
-        }
-        return derSchemaDAO;
-    }
-
-    protected DynRealmDAO dynRealmDAO() {
-        synchronized (this) {
-            if (dynRealmDAO == null) {
-                dynRealmDAO = ApplicationContextProvider.getApplicationContext().getBean(DynRealmDAO.class);
-            }
-        }
-        return dynRealmDAO;
-    }
 
     protected abstract AnyUtils init();
 
@@ -197,7 +172,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
             final PlainAttrValue attrValue,
             final boolean ignoreCaseMatch) {
 
-        PlainSchema schema = plainSchemaDAO().find(schemaKey);
+        PlainSchema schema = plainSchemaDAO.find(schemaKey);
         if (schema == null) {
             LOG.error("Invalid schema '{}'", schemaKey);
             return Collections.<A>emptyList();
@@ -235,7 +210,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
             final PlainAttrValue attrUniqueValue,
             final boolean ignoreCaseMatch) {
 
-        PlainSchema schema = plainSchemaDAO().find(schemaKey);
+        PlainSchema schema = plainSchemaDAO.find(schemaKey);
         if (schema == null) {
             LOG.error("Invalid schema '{}'", schemaKey);
             return null;
@@ -334,7 +309,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
         for (int i = 0; i < identifiers.size(); i++) {
             if (!used.contains(identifiers.get(i))) {
                 // verify schema existence and get schema type
-                PlainSchema schema = plainSchemaDAO().find(identifiers.get(i));
+                PlainSchema schema = plainSchemaDAO.find(identifiers.get(i));
                 if (schema == null) {
                     LOG.error("Invalid schema '{}', ignoring", identifiers.get(i));
                 } else {
@@ -394,7 +369,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
 
     @Override
     public List<A> findByDerAttrValue(final String schemaKey, final String value, final boolean ignoreCaseMatch) {
-        DerSchema schema = derSchemaDAO().find(schemaKey);
+        DerSchema schema = derSchemaDAO.find(schemaKey);
         if (schema == null) {
             LOG.error("Invalid schema '{}'", schemaKey);
             return Collections.<A>emptyList();
@@ -541,7 +516,7 @@ public abstract class AbstractAnyDAO<A extends Any<?>> extends AbstractDAO<A> im
                 ? (String) ((Object[]) resultKey)[0]
                 : ((String) resultKey)).
                 forEachOrdered((actualKey) -> {
-                    DynRealm dynRealm = dynRealmDAO().find(actualKey.toString());
+                    DynRealm dynRealm = dynRealmDAO.find(actualKey.toString());
                     if (dynRealm == null) {
                         LOG.error("Could not find dynRealm with id {}, even though returned by the native query",
                                 actualKey);

core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPAAnyObjectDAO.java
@@ -55,7 +55,7 @@ import org.apache.syncope.core.persistence.jpa.entity.anyobject.JPAAnyObject;
 import org.apache.syncope.core.persistence.jpa.entity.user.JPAURelationship;
 import org.apache.syncope.core.provisioning.api.event.AnyCreatedUpdatedEvent;
 import org.apache.syncope.core.provisioning.api.event.AnyDeletedEvent;
-import org.apache.syncope.core.spring.ApplicationContextProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -63,27 +63,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public class JPAAnyObjectDAO extends AbstractAnyDAO<AnyObject> implements AnyObjectDAO {
 
+    @Autowired
     private UserDAO userDAO;
 
+    @Autowired
     private GroupDAO groupDAO;
-
-    private UserDAO userDAO() {
-        synchronized (this) {
-            if (userDAO == null) {
-                userDAO = ApplicationContextProvider.getApplicationContext().getBean(UserDAO.class);
-            }
-        }
-        return userDAO;
-    }
-
-    private GroupDAO groupDAO() {
-        synchronized (this) {
-            if (groupDAO == null) {
-                groupDAO = ApplicationContextProvider.getApplicationContext().getBean(GroupDAO.class);
-            }
-        }
-        return groupDAO;
-    }
 
     @Override
     protected AnyUtils init() {
@@ -211,8 +195,8 @@ public class JPAAnyObjectDAO extends AbstractAnyDAO<AnyObject> implements AnyObj
         AnyObject merged = super.save(anyObject);
         publisher.publishEvent(new AnyCreatedUpdatedEvent<>(this, merged, AuthContextUtils.getDomain()));
 
-        Pair<Set<String>, Set<String>> dynGroupMembs = groupDAO().refreshDynMemberships(merged);
-        dynRealmDAO().refreshDynMemberships(merged);
+        Pair<Set<String>, Set<String>> dynGroupMembs = groupDAO.refreshDynMemberships(merged);
+        dynRealmDAO.refreshDynMemberships(merged);
 
         return Pair.of(merged, dynGroupMembs);
     }
@@ -247,8 +231,8 @@ public class JPAAnyObjectDAO extends AbstractAnyDAO<AnyObject> implements AnyObj
 
     @Override
     public void delete(final AnyObject anyObject) {
-        groupDAO().removeDynMemberships(anyObject);
-        dynRealmDAO().removeDynMemberships(anyObject.getKey());
+        groupDAO.removeDynMemberships(anyObject);
+        dynRealmDAO.removeDynMemberships(anyObject.getKey());
 
         findARelationships(anyObject).forEach(relationship -> {
             relationship.getLeftEnd().getRelationships().remove(relationship);
@@ -258,7 +242,7 @@ public class JPAAnyObjectDAO extends AbstractAnyDAO<AnyObject> implements AnyObj
         });
         findURelationships(anyObject).forEach(relationship -> {
             relationship.getLeftEnd().getRelationships().remove(relationship);
-            userDAO().save(relationship.getLeftEnd());
+            userDAO.save(relationship.getLeftEnd());
 
             entityManager().remove(relationship);
         });
@@ -281,7 +265,7 @@ public class JPAAnyObjectDAO extends AbstractAnyDAO<AnyObject> implements AnyObj
                 ? (String) ((Object[]) resultKey)[0]
                 : ((String) resultKey)).
                 forEachOrdered(actualKey -> {
-                    Group group = groupDAO().find(actualKey.toString());
+                    Group group = groupDAO.find(actualKey.toString());
                     if (group == null) {
                         LOG.error("Could not find group with id {}, even though returned by the native query",
                                 actualKey);

core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPADynRealmDAO.java
@@ -47,24 +47,16 @@ public class JPADynRealmDAO extends AbstractDAO<DynRealm> implements DynRealmDAO
     @Autowired
     private ApplicationEventPublisher publisher;
 
+    @Autowired
     private AnySearchDAO searchDAO;
 
     private AnySearchDAO jpaAnySearchDAO;
 
-    private AnySearchDAO searchDAO() {
-        synchronized (this) {
-            if (searchDAO == null) {
-                searchDAO = ApplicationContextProvider.getApplicationContext().getBean(AnySearchDAO.class);
-            }
-        }
-        return searchDAO;
-    }
-
     private AnySearchDAO jpaAnySearchDAO() {
         synchronized (this) {
             if (jpaAnySearchDAO == null) {
-                if (AopUtils.getTargetClass(searchDAO()).equals(JPAAnySearchDAO.class)) {
-                    jpaAnySearchDAO = searchDAO();
+                if (AopUtils.getTargetClass(searchDAO).equals(JPAAnySearchDAO.class)) {
+                    jpaAnySearchDAO = searchDAO;
                 } else {
                     jpaAnySearchDAO = (AnySearchDAO) ApplicationContextProvider.getBeanFactory().
                             createBean(JPAAnySearchDAO.class, AbstractBeanDefinition.AUTOWIRE_BY_TYPE, true);

core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPAExternalResourceDAO.java
@@ -52,7 +52,6 @@ import org.apache.syncope.core.persistence.jpa.entity.resource.JPAExternalResour
 import org.apache.syncope.core.persistence.jpa.entity.resource.JPAMapping;
 import org.apache.syncope.core.persistence.jpa.entity.resource.JPAProvision;
 import org.apache.syncope.core.provisioning.api.ConnectorRegistry;
-import org.apache.syncope.core.spring.ApplicationContextProvider;
 import org.apache.syncope.core.spring.security.AuthContextUtils;
 import org.apache.syncope.core.spring.security.DelegatedAdministrationException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,94 +64,29 @@ public class JPAExternalResourceDAO extends AbstractDAO<ExternalResource> implem
     @Autowired
     private ConnectorRegistry connRegistry;
 
+    @Autowired
     private TaskDAO taskDAO;
 
+    @Autowired
     private AnyObjectDAO anyObjectDAO;
 
+    @Autowired
     private UserDAO userDAO;
 
+    @Autowired
     private GroupDAO groupDAO;
 
+    @Autowired
     private PolicyDAO policyDAO;
 
+    @Autowired
     private VirSchemaDAO virSchemaDAO;
 
+    @Autowired
     private RealmDAO realmDAO;
 
+    @Autowired
     private ExternalResourceHistoryConfDAO externalResourceHistoryConfDAO;
-
-    private TaskDAO taskDAO() {
-        synchronized (this) {
-            if (taskDAO == null) {
-                taskDAO = ApplicationContextProvider.getApplicationContext().getBean(TaskDAO.class);
-            }
-        }
-        return taskDAO;
-    }
-
-    private AnyObjectDAO anyObjectDAO() {
-        synchronized (this) {
-            if (anyObjectDAO == null) {
-                anyObjectDAO = ApplicationContextProvider.getApplicationContext().getBean(AnyObjectDAO.class);
-            }
-        }
-        return anyObjectDAO;
-    }
-
-    private UserDAO userDAO() {
-        synchronized (this) {
-            if (userDAO == null) {
-                userDAO = ApplicationContextProvider.getApplicationContext().getBean(UserDAO.class);
-            }
-        }
-        return userDAO;
-    }
-
-    private GroupDAO groupDAO() {
-        synchronized (this) {
-            if (groupDAO == null) {
-                groupDAO = ApplicationContextProvider.getApplicationContext().getBean(GroupDAO.class);
-            }
-        }
-        return groupDAO;
-    }
-
-    private PolicyDAO policyDAO() {
-        synchronized (this) {
-            if (policyDAO == null) {
-                policyDAO = ApplicationContextProvider.getApplicationContext().getBean(PolicyDAO.class);
-            }
-        }
-        return policyDAO;
-    }
-
-    private VirSchemaDAO virSchemaDAO() {
-        synchronized (this) {
-            if (virSchemaDAO == null) {
-                virSchemaDAO = ApplicationContextProvider.getApplicationContext().getBean(VirSchemaDAO.class);
-            }
-        }
-        return virSchemaDAO;
-    }
-
-    private RealmDAO realmDAO() {
-        synchronized (this) {
-            if (realmDAO == null) {
-                realmDAO = ApplicationContextProvider.getApplicationContext().getBean(RealmDAO.class);
-            }
-        }
-        return realmDAO;
-    }
-
-    private ExternalResourceHistoryConfDAO externalResourceHistoryConfDAO() {
-        synchronized (this) {
-            if (externalResourceHistoryConfDAO == null) {
-                externalResourceHistoryConfDAO = ApplicationContextProvider.getApplicationContext().
-                        getBean(ExternalResourceHistoryConfDAO.class);
-            }
-        }
-        return externalResourceHistoryConfDAO;
-    }
 
     @Override
     public int count() {
@@ -302,19 +236,19 @@ public class JPAExternalResourceDAO extends AbstractDAO<ExternalResource> implem
             return;
         }
 
-        taskDAO().deleteAll(resource, TaskType.PROPAGATION);
-        taskDAO().deleteAll(resource, TaskType.PULL);
-        taskDAO().deleteAll(resource, TaskType.PUSH);
+        taskDAO.deleteAll(resource, TaskType.PROPAGATION);
+        taskDAO.deleteAll(resource, TaskType.PULL);
+        taskDAO.deleteAll(resource, TaskType.PUSH);
 
-        realmDAO().findByResource(resource).
+        realmDAO.findByResource(resource).
                 forEach(realm -> realm.getResources().remove(resource));
-        anyObjectDAO().findByResource(resource).
+        anyObjectDAO.findByResource(resource).
                 forEach(anyObject -> anyObject.getResources().remove(resource));
-        userDAO().findByResource(resource).
+        userDAO.findByResource(resource).
                 forEach(user -> user.getResources().remove(resource));
-        groupDAO().findByResource(resource).
+        groupDAO.findByResource(resource).
                 forEach(group -> group.getResources().remove(resource));
-        policyDAO().findByResource(resource).
+        policyDAO.findByResource(resource).
                 forEach(policy -> policy.getResources().remove(resource));
 
         resource.getProvisions().stream().
@@ -327,10 +261,10 @@ public class JPAExternalResourceDAO extends AbstractDAO<ExternalResource> implem
                     provision.setMapping(null);
                     provision.setResource(null);
                 }).
-                forEach(provision -> virSchemaDAO().findByProvision(provision).
-                forEach(schema -> virSchemaDAO().delete(schema.getKey())));
+                forEach(provision -> virSchemaDAO.findByProvision(provision).
+                forEach(schema -> virSchemaDAO.delete(schema.getKey())));
 
-        externalResourceHistoryConfDAO().deleteByEntity(resource);
+        externalResourceHistoryConfDAO.deleteByEntity(resource);
 
         if (resource.getConnector() != null && resource.getConnector().getResources() != null
                 && !resource.getConnector().getResources().isEmpty()) {

commit b3c11d2d8f812fe51f0e3fd74626d5f6629ac457

[SYNCOPE-1659] Fixing issue introduced by d2524ea for SYNCOPE-1392

core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/data/SchemaDataBinderImpl.java
@@ -313,7 +313,7 @@ public class SchemaDataBinderImpl implements SchemaDataBinder {
     private VirSchema fill(final VirSchema schema, final VirSchemaTO schemaTO) {
         schema.setKey(schemaTO.getKey());
         schema.setExtAttrName(schemaTO.getExtAttrName());
-        schema.setReadonly(schema.isReadonly());
+        schema.setReadonly(schemaTO.isReadonly());
 
         labels(schemaTO, schema);
 