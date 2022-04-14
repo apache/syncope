38235682e5915f34a02141a815194f65b15600c7
Francesco Chicchiriccò <ilgrosso@apache.org>
2022-04-14 16:02:30 +0200

    More fixes

--- core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPAEntityCacheDAO.java
+++ core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPAEntityCacheDAO.java
@@ -128,6 +128,7 @@ public class JPAEntityCacheDAO extends AbstractDAO<Entity> implements EntityCach
         queryStatistics().reset();
     }
 
+    @Override
     public void evict(final Class<? extends Entity> entityClass, final String key) {
         OpenJPAEntityManagerFactory emf = OpenJPAPersistence.cast(entityManagerFactory());
 
