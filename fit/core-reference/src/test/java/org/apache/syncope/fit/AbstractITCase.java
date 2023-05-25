5e4b67102253a74dee3edd4bdf679ab056936ae1
Francesco Chicchiriccò <ilgrosso@apache.org>
2023-05-25 12:14:30 +0200

    Fix IT cases

--- fit/core-reference/src/test/java/org/apache/syncope/fit/AbstractITCase.java
+++ fit/core-reference/src/test/java/org/apache/syncope/fit/AbstractITCase.java
@@ -394,7 +394,7 @@ public abstract class AbstractITCase {
     protected static boolean IS_ELASTICSEARCH_ENABLED = false;
 
     @BeforeAll
-    public static void securitySetup() {
+    public static void anonymousSetup() throws IOException {
         try (InputStream propStream = AbstractITCase.class.getResourceAsStream("/core.properties")) {
             Properties props = new Properties();
             props.load(propStream);
@@ -420,6 +420,20 @@ public abstract class AbstractITCase {
         U2F_REGISTRATION_SERVICE = ANONYMOUS_CLIENT.getService(U2FRegistrationService.class);
         WEBAUTHN_REGISTRATION_SERVICE = ANONYMOUS_CLIENT.getService(WebAuthnRegistrationService.class);
         IMPERSONATION_SERVICE = ANONYMOUS_CLIENT.getService(ImpersonationService.class);
+
+        JsonNode beans = JSON_MAPPER.readTree(
+                (InputStream) WebClient.create(
+                        StringUtils.substringBeforeLast(ADDRESS, "/") + "/actuator/beans",
+                        ANONYMOUS_UNAME,
+                        ANONYMOUS_KEY,
+                        null).
+                        accept(MediaType.APPLICATION_JSON).get().getEntity());
+
+        JsonNode uwfAdapter = beans.findValues("uwfAdapter").get(0);
+        IS_FLOWABLE_ENABLED = uwfAdapter.get("resource").asText().contains("Flowable");
+
+        JsonNode anySearchDAO = beans.findValues("anySearchDAO").get(0);
+        IS_ELASTICSEARCH_ENABLED = anySearchDAO.get("type").asText().contains("Elasticsearch");
     }
 
     @BeforeAll
@@ -478,19 +492,6 @@ public abstract class AbstractITCase {
         AUTH_PROFILE_SERVICE = ADMIN_CLIENT.getService(AuthProfileService.class);
         OIDC_JWKS_SERVICE = ADMIN_CLIENT.getService(OIDCJWKSService.class);
         WA_CONFIG_SERVICE = ADMIN_CLIENT.getService(WAConfigService.class);
-    }
-
-    @BeforeAll
-    public static void actuatorInfoSetup() throws IOException {
-        JsonNode beans = JSON_MAPPER.readTree(
-                (InputStream) WebClient.create(StringUtils.substringBeforeLast(ADDRESS, "/") + "/actuator/beans").
-                        accept(MediaType.APPLICATION_JSON).get().getEntity());
-
-        JsonNode uwfAdapter = beans.findValues("uwfAdapter").get(0);
-        IS_FLOWABLE_ENABLED = uwfAdapter.get("resource").asText().contains("Flowable");
-
-        JsonNode anySearchDAO = beans.findValues("anySearchDAO").get(0);
-        IS_ELASTICSEARCH_ENABLED = anySearchDAO.get("type").asText().contains("Elasticsearch");
     }
 
     protected static String getUUIDString() {
