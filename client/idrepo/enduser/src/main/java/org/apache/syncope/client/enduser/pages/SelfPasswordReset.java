16bf6eb0b0381ebab2aef21de2fa43b04fb6cf1a
Fabio Martelli <fmartelli@apache.org>
2023-10-03 12:12:31 +0200

    [SYNCOPE-1778] replaces AjaxFormComponentUpdatingBehavior with IndicatorAjaxFormComponentUpdatingBehavior (#526)

--- client/idrepo/enduser/src/main/java/org/apache/syncope/client/enduser/pages/SelfPasswordReset.java
+++ client/idrepo/enduser/src/main/java/org/apache/syncope/client/enduser/pages/SelfPasswordReset.java
@@ -25,6 +25,7 @@ import org.apache.syncope.client.enduser.panels.captcha.CaptchaPanel;
 import org.apache.syncope.client.enduser.rest.SecurityQuestionRestClient;
 import org.apache.syncope.client.enduser.rest.UserSelfRestClient;
 import org.apache.syncope.client.ui.commons.Constants;
+import org.apache.syncope.client.ui.commons.ajax.form.IndicatorAjaxFormComponentUpdatingBehavior;
 import org.apache.syncope.client.ui.commons.panels.CardPanel;
 import org.apache.syncope.common.lib.SyncopeClientException;
 import org.apache.syncope.common.lib.to.SecurityQuestionTO;
@@ -151,7 +152,7 @@ public class SelfPasswordReset extends BasePage {
 
             TextField<String> username = new TextField<>("username",
                     new PropertyModel<>(SelfPasswordReset.this, "usernameValue"), String.class);
-            username.add(new AjaxFormComponentUpdatingBehavior(Constants.ON_BLUR) {
+            username.add(new IndicatorAjaxFormComponentUpdatingBehavior(Constants.ON_BLUR) {
 
                 private static final long serialVersionUID = -1107858522700306810L;
 
