be6263fa540b7ae3d917c77f9e4504a8500d2aa2
sgarofalo <samuel.garofalo@protonmail.com>
2023-10-25 14:31:43 +0200

    fix imports

--- client/am/console/src/main/java/org/apache/syncope/client/console/clientapps/ClientAppModalPanelBuilder.java
+++ client/am/console/src/main/java/org/apache/syncope/client/console/clientapps/ClientAppModalPanelBuilder.java
@@ -18,7 +18,12 @@
  */
 package org.apache.syncope.client.console.clientapps;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.commons.lang3.RandomStringUtils;
