67d71c52933da979e529afb4f025c5417e3ef23a
Francesco Chicchiriccò <ilgrosso@apache.org>
2023-11-18 07:46:22 +0100

    Remove this flaky test from ES / OS

--- fit/core-reference/src/test/java/org/apache/syncope/fit/core/PullTaskITCase.java
+++ fit/core-reference/src/test/java/org/apache/syncope/fit/core/PullTaskITCase.java
@@ -1118,6 +1118,8 @@ public class PullTaskITCase extends AbstractTaskITCase {
 
     @Test
     public void issueSYNCOPE258() throws IOException {
+        assumeFalse(IS_EXT_SEARCH_ENABLED);
+
         // -----------------------------
         // Add a custom correlation rule
         // -----------------------------
