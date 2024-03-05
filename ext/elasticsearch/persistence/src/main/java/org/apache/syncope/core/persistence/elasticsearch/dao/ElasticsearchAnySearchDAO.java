af0af5a2272ce38c56b0c23a55474d4b4a935912
Francesco Chicchiriccò <ilgrosso@apache.org>
2024-03-05 15:27:42 +0100

    Fixing Elasticsearch and OpenSearch ILIKE / IEQ for values with underscore

--- ext/elasticsearch/persistence/src/main/java/org/apache/syncope/core/persistence/elasticsearch/dao/ElasticsearchAnySearchDAO.java
+++ ext/elasticsearch/persistence/src/main/java/org/apache/syncope/core/persistence/elasticsearch/dao/ElasticsearchAnySearchDAO.java
@@ -531,7 +531,7 @@ public class ElasticsearchAnySearchDAO extends AbstractAnySearchDAO {
 
             case ILIKE:
                 StringBuilder output = new StringBuilder();
-                for (char c : cond.getExpression().toLowerCase().toCharArray()) {
+                for (char c : cond.getExpression().toLowerCase().replace("\\_", "_").toCharArray()) {
                     if (c == '%') {
                         output.append(".*");
                     } else if (Character.isLetter(c)) {
@@ -549,7 +549,8 @@ public class ElasticsearchAnySearchDAO extends AbstractAnySearchDAO {
 
             case LIKE:
                 query = new Query.Builder().wildcard(QueryBuilders.wildcard().
-                        field(schema.getKey()).value(cond.getExpression().replace('%', '*')).build()).build();
+                        field(schema.getKey()).value(cond.getExpression().replace('%', '*').replace("\\_", "_")).
+                        build()).build();
                 break;
 
             case IEQ:
