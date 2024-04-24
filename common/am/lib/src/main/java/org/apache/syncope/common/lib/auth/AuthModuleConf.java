dd732de5fe9d38f0fec09662d7e15511adf8a850
Fabio Martelli <fmartelli@apache.org>
2024-04-24 17:48:57 +0200

    [SYNCOPE-1816] Enables LDAP parameters configuration for SPNEGO authentication module (#699)

--- common/am/lib/src/main/java/org/apache/syncope/common/lib/auth/AuthModuleConf.java
+++ common/am/lib/src/main/java/org/apache/syncope/common/lib/auth/AuthModuleConf.java
@@ -62,7 +62,7 @@ public interface AuthModuleConf extends BaseBean {
 
         Map<String, Object> map(AuthModuleTO authModule, SimpleMfaAuthModuleConf conf);
 
-        Map<String, Object> map(AuthModuleTO authModule, JcifsSpnegoAuthModuleConf conf);
+        Map<String, Object> map(AuthModuleTO authModule, SpnegoAuthModuleConf conf);
     }
 
     Map<String, Object> map(AuthModuleTO authModule, Mapper mapper);
