f5b65813b6dff5b5c786704c49b5a4dc6c2c9036
Francesco Chicchiriccò <ilgrosso@apache.org>
2024-07-26 11:16:35 +0200

    [SYNCOPE-1662] Introducing JSON support for MariaDB

---
+++ core/persistence-jpa-json/src/main/java/org/apache/syncope/core/persistence/jpa/MaJPAJSONPersistenceContext.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.syncope.core.persistence.jpa;
+
+import org.apache.syncope.core.persistence.api.attrvalue.validation.PlainAttrValidationManager;
+import org.apache.syncope.core.persistence.api.dao.AnyObjectDAO;
+import org.apache.syncope.core.persistence.api.dao.AnySearchDAO;
+import org.apache.syncope.core.persistence.api.dao.AuditConfDAO;
+import org.apache.syncope.core.persistence.api.dao.DynRealmDAO;
+import org.apache.syncope.core.persistence.api.dao.ExternalResourceDAO;
+import org.apache.syncope.core.persistence.api.dao.GroupDAO;
+import org.apache.syncope.core.persistence.api.dao.JPAJSONAnyDAO;
+import org.apache.syncope.core.persistence.api.dao.PlainAttrDAO;
+import org.apache.syncope.core.persistence.api.dao.PlainSchemaDAO;
+import org.apache.syncope.core.persistence.api.dao.RealmDAO;
+import org.apache.syncope.core.persistence.api.dao.UserDAO;
+import org.apache.syncope.core.persistence.api.entity.AnyUtilsFactory;
+import org.apache.syncope.core.persistence.api.entity.EntityFactory;
+import org.apache.syncope.core.persistence.jpa.dao.MaJPAJSONAnyDAO;
+import org.apache.syncope.core.persistence.jpa.dao.MaJPAJSONAnySearchDAO;
+import org.apache.syncope.core.persistence.jpa.dao.MaJPAJSONAuditConfDAO;
+import org.apache.syncope.core.persistence.jpa.dao.MaJPAJSONPlainSchemaDAO;
+import org.apache.syncope.core.persistence.jpa.entity.MaJPAJSONEntityFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+
+@ConditionalOnExpression("#{'${provisioning.quartz.sql}' matches '.*mariadb.*'}")
+public class MaJPAJSONPersistenceContext extends JPAJSONPersistenceContext {
+
+    @ConditionalOnMissingBean(name = "maJPAJSONEntityFactory")
+    @Bean
+    public EntityFactory entityFactory() {
+        return new MaJPAJSONEntityFactory();
+    }
+
+    @ConditionalOnMissingBean(name = "maJPAJSONAnyDAO")
+    @Bean
+    public JPAJSONAnyDAO anyDAO(final @Lazy PlainSchemaDAO plainSchemaDAO) {
+        return new MaJPAJSONAnyDAO(plainSchemaDAO);
+    }
+
+    @ConditionalOnMissingBean(name = "maJPAJSONAnySearchDAO")
+    @Bean
+    public AnySearchDAO anySearchDAO(
+            final @Lazy RealmDAO realmDAO,
+            final @Lazy DynRealmDAO dynRealmDAO,
+            final @Lazy UserDAO userDAO,
+            final @Lazy GroupDAO groupDAO,
+            final @Lazy AnyObjectDAO anyObjectDAO,
+            final @Lazy PlainSchemaDAO schemaDAO,
+            final @Lazy EntityFactory entityFactory,
+            final AnyUtilsFactory anyUtilsFactory,
+            final PlainAttrValidationManager validator) {
+
+        return new MaJPAJSONAnySearchDAO(
+                realmDAO,
+                dynRealmDAO,
+                userDAO,
+                groupDAO,
+                anyObjectDAO,
+                schemaDAO,
+                entityFactory,
+                anyUtilsFactory,
+                validator);
+    }
+
+    @ConditionalOnMissingBean(name = "maJPAJSONAuditConfDAO")
+    @Bean
+    public AuditConfDAO auditConfDAO() {
+        return new MaJPAJSONAuditConfDAO();
+    }
+
+    @ConditionalOnMissingBean(name = "maJPAJSONPlainSchemaDAO")
+    @Bean
+    public PlainSchemaDAO plainSchemaDAO(
+            final AnyUtilsFactory anyUtilsFactory,
+            final @Lazy PlainAttrDAO plainAttrDAO,
+            final @Lazy ExternalResourceDAO resourceDAO) {
+
+        return new MaJPAJSONPlainSchemaDAO(anyUtilsFactory, plainAttrDAO, resourceDAO);
+    }
+}
---
+++ core/persistence-jpa-json/src/main/java/org/apache/syncope/core/persistence/jpa/dao/MaJPAJSONAnySearchDAO.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.syncope.core.persistence.jpa.dao;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.syncope.core.persistence.api.attrvalue.validation.PlainAttrValidationManager;
+import org.apache.syncope.core.persistence.api.dao.AnyObjectDAO;
+import org.apache.syncope.core.persistence.api.dao.DynRealmDAO;
+import org.apache.syncope.core.persistence.api.dao.GroupDAO;
+import org.apache.syncope.core.persistence.api.dao.PlainSchemaDAO;
+import org.apache.syncope.core.persistence.api.dao.RealmDAO;
+import org.apache.syncope.core.persistence.api.dao.UserDAO;
+import org.apache.syncope.core.persistence.api.dao.search.AnyCond;
+import org.apache.syncope.core.persistence.api.dao.search.AttrCond;
+import org.apache.syncope.core.persistence.api.entity.AnyUtilsFactory;
+import org.apache.syncope.core.persistence.api.entity.EntityFactory;
+import org.apache.syncope.core.persistence.api.entity.JSONPlainAttr;
+import org.apache.syncope.core.persistence.api.entity.PlainAttr;
+import org.apache.syncope.core.persistence.api.entity.PlainAttrUniqueValue;
+import org.apache.syncope.core.persistence.api.entity.PlainAttrValue;
+import org.apache.syncope.core.persistence.api.entity.PlainSchema;
+import org.apache.syncope.core.provisioning.api.serialization.POJOHelper;
+
+public class MaJPAJSONAnySearchDAO extends JPAAnySearchDAO {
+
+    public MaJPAJSONAnySearchDAO(
+            final RealmDAO realmDAO,
+            final DynRealmDAO dynRealmDAO,
+            final UserDAO userDAO,
+            final GroupDAO groupDAO,
+            final AnyObjectDAO anyObjectDAO,
+            final PlainSchemaDAO schemaDAO,
+            final EntityFactory entityFactory,
+            final AnyUtilsFactory anyUtilsFactory,
+            final PlainAttrValidationManager validator) {
+
+        super(
+                realmDAO,
+                dynRealmDAO,
+                userDAO,
+                groupDAO,
+                anyObjectDAO,
+                schemaDAO,
+                entityFactory,
+                anyUtilsFactory,
+                validator);
+    }
+
+    @Override
+    protected String getQuery(
+            final AttrCond cond,
+            final boolean not,
+            final List<Object> parameters,
+            final SearchSupport svs) {
+
+        Pair<PlainSchema, PlainAttrValue> checked = check(cond, svs.anyTypeKind);
+
+        // normalize NULL / NOT NULL checks
+        if (not) {
+            if (cond.getType() == AttrCond.Type.ISNULL) {
+                cond.setType(AttrCond.Type.ISNOTNULL);
+            } else if (cond.getType() == AttrCond.Type.ISNOTNULL) {
+                cond.setType(AttrCond.Type.ISNULL);
+            }
+        }
+
+        StringBuilder query =
+                new StringBuilder("SELECT DISTINCT any_id FROM ").append(svs.field().name).append(" WHERE ");
+        switch (cond.getType()) {
+            case ISNOTNULL:
+                query.append("JSON_SEARCH(plainAttrs, 'one', '").
+                        append(checked.getLeft().getKey()).
+                        append("', NULL, '$[*].schema') IS NOT NULL");
+                break;
+
+            case ISNULL:
+                query.append("JSON_SEARCH(plainAttrs, 'one', '").
+                        append(checked.getLeft().getKey()).
+                        append("', NULL, '$[*].schema') IS NULL");
+                break;
+
+            default:
+                if (!not && cond.getType() == AttrCond.Type.EQ) {
+                    PlainAttr<?> container = anyUtilsFactory.getInstance(svs.anyTypeKind).newPlainAttr();
+                    container.setSchema(checked.getLeft());
+                    if (checked.getRight() instanceof PlainAttrUniqueValue) {
+                        container.setUniqueValue((PlainAttrUniqueValue) checked.getRight());
+                    } else {
+                        ((JSONPlainAttr) container).add(checked.getRight());
+                    }
+
+                    query.append("JSON_CONTAINS(plainAttrs, '").
+                            append(POJOHelper.serialize(List.of(container)).replace("'", "''")).
+                            append("')");
+                } else {
+                    query = new StringBuilder("SELECT DISTINCT any_id FROM ");
+                    if (not && !(cond instanceof AnyCond) && checked.getLeft().isMultivalue()) {
+                        query.append(svs.field().name).append(" WHERE ");
+                    } else {
+                        query.append((checked.getLeft().isUniqueConstraint()
+                                ? svs.asSearchViewSupport().uniqueAttr().name
+                                : svs.asSearchViewSupport().attr().name)).
+                                append(" WHERE schema_id='").append(checked.getLeft().getKey());
+                    }
+
+                    Optional.ofNullable(checked.getRight().getDateValue()).
+                            map(DateTimeFormatter.ISO_OFFSET_DATE_TIME::format).
+                            ifPresent(formatted -> {
+                                checked.getRight().setDateValue(null);
+                                checked.getRight().setStringValue(formatted);
+                            });
+
+                    fillAttrQuery(query, checked.getRight(), checked.getLeft(), cond, not, parameters, svs);
+                }
+        }
+
+        return query.toString();
+    }
+}
