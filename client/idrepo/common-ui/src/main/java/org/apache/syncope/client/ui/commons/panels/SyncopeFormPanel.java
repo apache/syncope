1d3108472cb4e61405f7a77eb4c5ae30575f19e5
alberto bogi <35602311+alberto-bogi@users.noreply.github.com>
2025-01-29 15:58:06 +0100

    [SYNCOPE-1858] Define dropdown form property without default value on a macro   (#970)

--- client/idrepo/common-ui/src/main/java/org/apache/syncope/client/ui/commons/panels/SyncopeFormPanel.java
+++ client/idrepo/common-ui/src/main/java/org/apache/syncope/client/ui/commons/panels/SyncopeFormPanel.java
@@ -165,7 +165,7 @@ public class SyncopeFormPanel<F extends SyncopeForm> extends Panel {
                                 public List<String> getObject() {
                                     return Optional.ofNullable(prop.getValue()).
                                             map(v -> List.of(v.split(";"))).
-                                            orElse(null);
+                                            orElse(List.of());
                                 }
 
                                 @Override
