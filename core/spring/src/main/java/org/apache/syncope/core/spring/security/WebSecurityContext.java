562308297719520e90dae155ba3a39e96038e868
Francesco Chicchiriccò <ilgrosso@apache.org>
2025-07-10 13:32:42 +0200

    Fix build

--- core/spring/src/main/java/org/apache/syncope/core/spring/security/WebSecurityContext.java
+++ core/spring/src/main/java/org/apache/syncope/core/spring/security/WebSecurityContext.java
@@ -51,7 +51,7 @@ import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
-import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
@@ -106,9 +106,9 @@ public class WebSecurityContext {
         http.addFilterBefore(mustChangePasswordFilter, AuthorizationFilter.class);
 
         http.authorizeHttpRequests(customizer -> customizer.
-                requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/actuator/**")).
+                requestMatchers(AntPathRequestMatcher.antMatcher("/actuator/**")).
                 hasAuthority(IdRepoEntitlement.ANONYMOUS).
-                requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/**")).permitAll());
+                requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll());
         http.securityContext(AbstractHttpConfigurer::disable);
         http.sessionManagement(AbstractHttpConfigurer::disable);
         http.headers(AbstractHttpConfigurer::disable);
