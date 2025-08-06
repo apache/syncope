07b0d27d7553bd2ead79ff9486b5ca01f69be0dd
Francesco Chicchiriccò <ilgrosso@apache.org>
2025-08-06 11:40:41 +0200

    [SYNCOPE-1904] Adding exceptions for value like username which looks like Base64 but it is not

--- client/idrepo/console/src/main/java/org/apache/syncope/client/console/panels/ParametersModalPanel.java
+++ client/idrepo/console/src/main/java/org/apache/syncope/client/console/panels/ParametersModalPanel.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.text.ParseException;
 import java.util.Base64;
+import java.util.Set;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import org.apache.commons.lang3.time.DateFormatUtils;
@@ -43,11 +44,13 @@ public class ParametersModalPanel extends AbstractModalPanel<ConfParam> {
 
     private static final long serialVersionUID = 4024126489500665435L;
 
-    private static final JsonMapper JSON_MAPPER = JsonMapper.builder().findAndAddModules().build();
+    protected static final Set<String> BASE64_EXCEPTIONS = Set.of("username");
 
-    private static final SAXParserFactory SAX_PARSER_FACTORY = SAXParserFactory.newInstance();
+    protected static final JsonMapper JSON_MAPPER = JsonMapper.builder().findAndAddModules().build();
 
-    private static boolean isDate(final String value) {
+    protected static final SAXParserFactory SAX_PARSER_FACTORY = SAXParserFactory.newInstance();
+
+    protected static boolean isDate(final String value) {
         try {
             DateFormatUtils.ISO_8601_EXTENDED_DATETIME_TIME_ZONE_FORMAT.parse(value);
             return true;
@@ -56,16 +59,16 @@ public class ParametersModalPanel extends AbstractModalPanel<ConfParam> {
         }
     }
 
-    private static boolean isBase64(final String value) {
+    protected static boolean isBase64(final String value) {
         try {
             Base64.getDecoder().decode(value);
-            return value.length() % 4 == 0;
+            return value.length() % 4 == 0 && !BASE64_EXCEPTIONS.contains(value);
         } catch (IllegalArgumentException iae) {
             return false;
         }
     }
 
-    private static boolean isJSON(final String value) {
+    protected static boolean isJSON(final String value) {
         try {
             JSON_MAPPER.readTree(value);
             return true;
@@ -74,7 +77,7 @@ public class ParametersModalPanel extends AbstractModalPanel<ConfParam> {
         }
     }
 
-    private static boolean isXML(final String value) {
+    protected static boolean isXML(final String value) {
         try {
             SAX_PARSER_FACTORY.newSAXParser().getXMLReader().parse(new InputSource(new StringReader(value)));
             return true;
@@ -83,7 +86,7 @@ public class ParametersModalPanel extends AbstractModalPanel<ConfParam> {
         }
     }
 
-    private static boolean isPEM(final String value) {
+    protected static boolean isPEM(final String value) {
         try (PemReader reader = new PemReader(new StringReader(value))) {
             return reader.readPemObject() != null;
         } catch (IOException e) {
@@ -91,7 +94,7 @@ public class ParametersModalPanel extends AbstractModalPanel<ConfParam> {
         }
     }
 
-    private final ParametersWizardPanel.ParametersForm form;
+    protected final ParametersWizardPanel.ParametersForm form;
 
     public ParametersModalPanel(
             final BaseModal<ConfParam> modal,
@@ -105,44 +108,39 @@ public class ParametersModalPanel extends AbstractModalPanel<ConfParam> {
         PlainSchemaTO schema = new PlainSchemaTO();
         schema.setMultivalue(param.isMultivalue());
         schema.setMimeType(MediaType.APPLICATION_OCTET_STREAM);
-        if (param.getSchema() != null) {
-            if (param.isInstance(Boolean.class)) {
-                schema.setType(AttrSchemaType.Boolean);
-            } else if (param.isInstance(Integer.class) || param.isInstance(Long.class)) {
-                schema.setType(AttrSchemaType.Long);
-            } else if (param.isInstance(Float.class) || param.isInstance(Double.class)) {
-                schema.setType(AttrSchemaType.Double);
-            } else // attempt to guess type from content: otherwise, it's bare String
-            if (!param.getValues().isEmpty()) {
-                // 1. is it Date?
-                if (isDate(param.getValues().getFirst().toString())) {
-                    schema.setType(AttrSchemaType.Date);
-                } else // 2. does it look like Base64?
-                if (isBase64(param.getValues().getFirst().toString())) {
-                    schema.setType(AttrSchemaType.Binary);
-                    String value = new String(Base64.getDecoder().decode(param.getValues().getFirst().toString()));
 
-                    // 3. is it JSON?
-                    if (isJSON(value)) {
-                        schema.setMimeType(MediaType.APPLICATION_JSON);
-                    } else // 4. is it XML?
-                    if (isXML(value)) {
-                        schema.setMimeType(MediaType.APPLICATION_XML);
-                    } else // 5. is it PEM?
-                    if (isPEM(value)) {
-                        schema.setMimeType("application/x-pem-file");
-                    }
-                } else {
-                    schema.setType(AttrSchemaType.String);
+        if (param.isInstance(Boolean.class)) {
+            schema.setType(AttrSchemaType.Boolean);
+        } else if (param.isInstance(Integer.class) || param.isInstance(Long.class)) {
+            schema.setType(AttrSchemaType.Long);
+        } else if (param.isInstance(Float.class) || param.isInstance(Double.class)) {
+            schema.setType(AttrSchemaType.Double);
+        } else // attempt to guess type from content: otherwise, it's bare String
+        if (!param.getValues().isEmpty()) {
+            // 1. is it Date?
+            if (isDate(param.getValues().getFirst().toString())) {
+                schema.setType(AttrSchemaType.Date);
+            } else // 2. does it look like Base64?
+            if (isBase64(param.getValues().getFirst().toString())) {
+                schema.setType(AttrSchemaType.Binary);
+                String value = new String(Base64.getDecoder().decode(param.getValues().getFirst().toString()));
+
+                // 3. is it JSON?
+                if (isJSON(value)) {
+                    schema.setMimeType(MediaType.APPLICATION_JSON);
+                } else // 4. is it XML?
+                if (isXML(value)) {
+                    schema.setMimeType(MediaType.APPLICATION_XML);
+                } else // 5. is it PEM?
+                if (isPEM(value)) {
+                    schema.setMimeType("application/x-pem-file");
                 }
+            } else {
+                schema.setType(AttrSchemaType.String);
             }
         }
 
-        if (schema.getType() == AttrSchemaType.Binary) {
-            modal.size(Modal.Size.Extra_large);
-        } else {
-            modal.size(Modal.Size.Default);
-        }
+        modal.size(schema.getType() == AttrSchemaType.Binary ? Modal.Size.Extra_large : Modal.Size.Default);
 
         form = new ParametersWizardPanel.ParametersForm(schema, param);
         add(new ParametersWizardPanel(form, confParamOps, pageRef).build("parametersCreateWizardPanel", mode));
