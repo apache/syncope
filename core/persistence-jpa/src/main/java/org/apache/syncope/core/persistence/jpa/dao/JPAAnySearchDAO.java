051c91c497449ea39e4c6d3df3b9a14e816f24c1
Andrea Patricelli <andreapatricelli@apache.org>
2025-09-03 17:36:33 +0200

    [SYNCOPE-1906] Fixing order by on plain attribute (#1164)

--- core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPAAnySearchDAO.java
+++ core/persistence-jpa/src/main/java/org/apache/syncope/core/persistence/jpa/dao/JPAAnySearchDAO.java
@@ -1006,8 +1006,15 @@ public class JPAAnySearchDAO extends AbstractAnySearchDAO {
             obs.views.add(svs.asSearchViewSupport().attr());
 
             item.select = new StringBuilder().
-                    append(svs.asSearchViewSupport().attr().alias).append('.').append(key(schema.getType())).
-                    append(" AS ").append(fieldName).toString();
+                    append("( SELECT usa").append('.').append(key(schema.getType())).
+                    append(" FROM ").
+                    append(svs.asSearchViewSupport().attr().name).
+                    append(" usa WHERE usa.any_id = ").
+                    append(svs.asSearchViewSupport().attr().alias).
+                    append(".any_id").
+                    append(" AND usa.schema_id ='").append(fieldName).append("'").
+                    append(isOracle() ? " FETCH FIRST 1 ROWS ONLY " : " LIMIT 1").
+                    append(") AS ").append(fieldName).toString();
             item.where = new StringBuilder().
                     append(svs.asSearchViewSupport().attr().alias).
                     append(".schema_id='").append(fieldName).append("'").toString();
