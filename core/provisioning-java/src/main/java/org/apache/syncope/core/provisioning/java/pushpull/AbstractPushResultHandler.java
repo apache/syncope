52366e4b598c7bc4412d1063183fb4c51ef609fa
sgarofalo <samuel.garofalo@protonmail.com>
2025-09-30 10:00:31 +0200

    fix

--- core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/pushpull/AbstractPushResultHandler.java
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/pushpull/AbstractPushResultHandler.java
@@ -215,8 +215,8 @@ public abstract class AbstractPushResultHandler extends AbstractSyncopeResultHan
     }
 
     protected void copyDynMembershipConds(final Any<?> any, final AnyUR req) {
-        if (any instanceof Group &&
-                (((Group) any).getUDynMembership() != null || !((Group) any).getADynMemberships().isEmpty())) {
+        if (any instanceof Group
+                && (((Group) any).getUDynMembership() != null || !((Group) any).getADynMemberships().isEmpty())) {
             ((GroupUR) req).setUDynMembershipCond(((Group) any).getUDynMembership().getFIQLCond());
 
             ((Group) any).getADynMemberships().forEach(aDynGroupMembership ->
