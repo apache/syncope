8ff02bd8e2c079714f94cd12a1961df99e8700d8
Massimiliano Perrone <massimiliano.perrone@tirasa.net>
2026-06-04 06:07:23 +0200

    Avoid authentication error detail disclosure

--- core/spring/src/test/java/org/apache/syncope/core/spring/security/SyncopeBasicAuthenticationEntryPointTest.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/security/SyncopeBasicAuthenticationEntryPointTest.java
@@ -21,17 +21,29 @@ package org.apache.syncope.core.spring.security;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.syncope.common.rest.api.RESTHeaders;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 class SyncopeBasicAuthenticationEntryPointTest {
 
+    private SyncopeBasicAuthenticationEntryPoint entryPoint;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        entryPoint = new SyncopeBasicAuthenticationEntryPoint();
+        entryPoint.setRealmName("Apache Syncope authentication");
+        entryPoint.afterPropertiesSet();
+    }
+
     @Test
     void rateLimitAuthenticationExceptionReturnsTooManyRequests() throws Exception {
-        SyncopeBasicAuthenticationEntryPoint entryPoint = new SyncopeBasicAuthenticationEntryPoint();
         MockHttpServletResponse response = new MockHttpServletResponse();
 
         entryPoint.commence(
@@ -41,6 +53,49 @@ class SyncopeBasicAuthenticationEntryPointTest {
 
         assertEquals(HttpStatus.TOO_MANY_REQUESTS.value(), response.getStatus());
         assertEquals("30", response.getHeader(HttpHeaders.RETRY_AFTER));
-        assertEquals("Too many authentication failures", response.getHeader(RESTHeaders.ERROR_INFO));
+        assertEquals(SyncopeBasicAuthenticationEntryPoint.AUTHENTICATION_FAILED,
+                response.getHeader(RESTHeaders.ERROR_INFO));
+    }
+
+    @Test
+    public void badCredentialsExposeGenericErrorInfo() throws Exception {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        entryPoint.commence(
+                new MockHttpServletRequest(),
+                response,
+                new BadCredentialsException("rossini: invalid password provided"));
+
+        assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
+        assertEquals(SyncopeBasicAuthenticationEntryPoint.AUTHENTICATION_FAILED,
+                response.getHeader(RESTHeaders.ERROR_INFO));
+    }
+
+    @Test
+    public void missingUserExposeGenericErrorInfo() throws Exception {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        entryPoint.commence(
+                new MockHttpServletRequest(),
+                response,
+                new UsernameNotFoundException("not-a-user"));
+
+        assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
+        assertEquals(SyncopeBasicAuthenticationEntryPoint.AUTHENTICATION_FAILED,
+                response.getHeader(RESTHeaders.ERROR_INFO));
+    }
+
+    @Test
+    public void disabledUserExposeGenericErrorInfo() throws Exception {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        entryPoint.commence(
+                new MockHttpServletRequest(),
+                response,
+                new DisabledException("User rossini is suspended"));
+
+        assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
+        assertEquals(SyncopeBasicAuthenticationEntryPoint.AUTHENTICATION_FAILED,
+                response.getHeader(RESTHeaders.ERROR_INFO));
     }
 }
