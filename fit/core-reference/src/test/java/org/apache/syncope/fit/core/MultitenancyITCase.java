076bb6dc70290ed1cea64b2478dbd055e29b136b
Massimiliano Perrone <massimiliano.perrone@tirasa.net>
2026-06-15 13:12:01 +0200

    [SYNCOPE-1973] Align FIT expectations with generic authentication errors

--- fit/core-reference/src/test/java/org/apache/syncope/fit/core/MultitenancyITCase.java
+++ fit/core-reference/src/test/java/org/apache/syncope/fit/core/MultitenancyITCase.java
@@ -21,7 +21,6 @@ package org.apache.syncope.fit.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
