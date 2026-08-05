c5f549382e379434043633261f2c80f44ba930ba
Francesco Chicchiriccò <ilgrosso@apache.org>
2026-08-05 11:29:59 +0200

    Fixing after Groovy update

--- core/spring/src/test/java/org/apache/syncope/core/spring/implementation/GroovySandboxTest.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/implementation/GroovySandboxTest.java
@@ -18,6 +18,7 @@
  */
 package org.apache.syncope.core.spring.implementation;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -25,6 +26,7 @@ import static org.mockito.Mockito.when;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -35,6 +37,10 @@ import org.apache.syncope.common.lib.types.ImplementationEngine;
 import org.apache.syncope.core.persistence.api.entity.Implementation;
 import org.apache.syncope.core.provisioning.api.macro.MacroActions;
 import org.apache.syncope.core.spring.SpringTestConfiguration;
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.common.script.ScriptExecutor;
+import org.identityconnectors.common.script.ScriptExecutorFactory;
+import org.identityconnectors.framework.common.objects.ObjectClass;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
@@ -47,6 +53,23 @@ class GroovySandboxTest {
 
     @TempDir
     private Path tempDir;
+
+    @Test
+    void script() {
+        String script =
+                """
+                log.info("Entering {0} Script for {1}", action, objectClass)
+                """;
+        ScriptExecutor scriptExecutor = ScriptExecutorFactory.newInstance("GROOVY").
+                newScriptExecutor(getClass().getClassLoader(), script, true);
+
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("objectClass", ObjectClass.ACCOUNT_NAME);
+        arguments.put("action", "SEARCH");
+        arguments.put("log", Log.getLog(GroovySandboxTest.class));
+
+        assertDoesNotThrow(() -> scriptExecutor.execute(arguments));
+    }
 
     @Test
     void processBuilder() throws Exception {
