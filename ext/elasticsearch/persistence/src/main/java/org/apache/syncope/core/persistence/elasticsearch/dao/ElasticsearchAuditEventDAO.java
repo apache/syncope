7652078269dfebbe20cfa38d1d0b7da759d6fa14
Francesco Chicchiriccò <ilgrosso@apache.org>
2026-08-20 11:39:18 +0200

    Ensuring to escape Audit Events search parameters for LIKE

--- ext/elasticsearch/persistence/src/main/java/org/apache/syncope/core/persistence/elasticsearch/dao/ElasticsearchAuditEventDAO.java
+++ ext/elasticsearch/persistence/src/main/java/org/apache/syncope/core/persistence/elasticsearch/dao/ElasticsearchAuditEventDAO.java
@@ -41,6 +41,7 @@ import org.apache.syncope.common.lib.to.AuditEventTO;
 import org.apache.syncope.common.lib.types.OpEvent;
 import org.apache.syncope.core.persistence.api.dao.AuditEventDAO;
 import org.apache.syncope.core.persistence.api.entity.AuditEvent;
+import org.apache.syncope.core.persistence.common.dao.AbstractAuditEventDAO;
 import org.apache.syncope.core.spring.security.AuthContextUtils;
 import org.apache.syncope.ext.elasticsearch.client.ElasticsearchIndexManager;
 import org.apache.syncope.ext.elasticsearch.client.ElasticsearchUtils;
@@ -50,7 +51,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.util.CollectionUtils;
 
-public class ElasticsearchAuditEventDAO implements AuditEventDAO {
+public class ElasticsearchAuditEventDAO extends AbstractAuditEventDAO implements AuditEventDAO {
 
     protected static final Logger LOG = LoggerFactory.getLogger(AuditEventDAO.class);
 
@@ -95,6 +96,8 @@ public class ElasticsearchAuditEventDAO implements AuditEventDAO {
         List<Query> queries = new ArrayList<>();
 
         if (entityKey != null) {
+            checkEntityKey(entityKey);
+
             queries.add(new Query.Builder().
                     multiMatch(QueryBuilders.multiMatch().
                             fields("before", "inputs", "output", "throwable").
@@ -123,7 +126,7 @@ public class ElasticsearchAuditEventDAO implements AuditEventDAO {
 
         queries.add(new Query.Builder().regexp(QueryBuilders.regexp().
                 field("opEvent").
-                value(OpEvent.toString(type, category, subcategory, op, outcome).
+                value(toOpEvent(type, category, subcategory, op, outcome).
                         replace("[]", "[.*]").
                         replace("[", "\\[").
                         replace("]", "\\]")).build()).
