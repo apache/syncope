0eded4829d2792767816dcea4b13af04a17d89b2
Francesco Chicchiriccò <ilgrosso@apache.org>
2026-08-25 10:51:15 +0200

    Fixing ConnInstance confidential property masking for audit events

--- core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/DefaultAuditManager.java
+++ core/provisioning-java/src/main/java/org/apache/syncope/core/provisioning/java/DefaultAuditManager.java
@@ -114,7 +114,7 @@ public class DefaultAuditManager implements AuditManager {
 
             case ConnInstanceTO connInstanceTO -> {
                 ConnInstanceTO clone = SerializationUtils.clone(connInstanceTO);
-                maskSensitive(connInstanceTO.getConf());
+                maskSensitive(clone.getConf());
                 yield clone;
             }
 
