b8b97e0dccbc9e492ca0f82e0a953ff45d1e7354
Francesco Chicchiriccò <ilgrosso@apache.org>
2026-09-09 08:58:26 +0200

    Upgrading WicketStuff and Jakarta Faces

--- core/persistence-common/src/main/java/org/apache/syncope/core/persistence/common/validation/PlainSchemaValidator.java
+++ core/persistence-common/src/main/java/org/apache/syncope/core/persistence/common/validation/PlainSchemaValidator.java
@@ -18,8 +18,11 @@
  */
 package org.apache.syncope.core.persistence.common.validation;
 
+import static org.apache.syncope.core.persistence.api.entity.PlainAttrValue.SPRING_ENV_PROPERTY;
+
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Optional;
+import org.apache.syncope.common.lib.SyncopeConstants;
 import org.apache.syncope.common.lib.types.CipherAlgorithm;
 import org.apache.syncope.common.lib.types.EntityViolationType;
 import org.apache.syncope.core.persistence.api.entity.PlainSchema;
@@ -57,18 +60,19 @@ public class PlainSchemaValidator extends AbstractValidator<PlainSchemaCheck, Pl
                             addPropertyNode("secretKey").addPropertyNode("cipherAlgorithm").addConstraintViolation();
                     return false;
                 }
-                if (schema.getCipherAlgorithm() == CipherAlgorithm.AES) {
-                    if (Optional.ofNullable(schema.getSecretKey()).
-                            map(key -> key.length() != 16 && key.length() != 24 && key.length() != 32).
-                            orElse(true)) {
+                if (schema.getCipherAlgorithm() == CipherAlgorithm.AES
+                        && !SyncopeConstants.ENCRYPTED_DECODE_CONVERSION_PATTERN.equals(schema.getConversionPattern())
+                        && !SPRING_ENV_PROPERTY.matcher(schema.getSecretKey()).matches()
+                        && Optional.ofNullable(schema.getSecretKey()).
+                                map(key -> key.length() != 16 && key.length() != 24 && key.length() != 32).
+                                orElse(true)) {
 
-                        context.disableDefaultConstraintViolation();
-                        context.buildConstraintViolationWithTemplate(
-                                getTemplate(EntityViolationType.InvalidSchema,
-                                        "SecretKey length requirements not met for AES: must be either 16, 24 or 32")).
-                                addPropertyNode("secretKey").addConstraintViolation();
-                        return false;
-                    }
+                    context.disableDefaultConstraintViolation();
+                    context.buildConstraintViolationWithTemplate(
+                            getTemplate(EntityViolationType.InvalidSchema,
+                                    "SecretKey length requirements not met for AES: must be 16 24 or 32")).
+                            addPropertyNode("secretKey").addConstraintViolation();
+                    return false;
                 }
             }
 
