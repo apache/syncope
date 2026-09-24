51188c60edafa79e165ebe37c18d894dbe614094
TatoniMatteo <tatonimatteo@gmail.com>
2026-09-24 12:13:56 +0200

    Finalize theme switcher: - Align enduser login - Fix accesskey handling - Update documentation

--- client/idrepo/common-ui/src/main/resources/META-INF/resources/ui-commons/js/accessibility.js
+++ client/idrepo/common-ui/src/main/resources/META-INF/resources/ui-commons/js/accessibility.js
@@ -23,32 +23,17 @@ $(function () {
         localStorage.setItem(fontSizePreference, increasedFont);
     };
 
-    switchFontSize();
-
     const toggleFontSize = () => {
         increasedFont = !increasedFont;
         switchFontSize();
     };
 
-    $('#change_fontSize').off('.acc_font').on({
-        'click.acc_font': function () {
+    switchFontSize();
+
+    $('#change_fontSize')
+        .off('.acc_font')
+        .on('click.acc_font', function () {
             toggleFontSize();
             return false;
-        },
-
-        'keydown.acc_font': function (event) {
-            if (event.key === 'Enter' || event.key === ' ') {
-                toggleFontSize();
-                event.preventDefault();
-                return false;
-            }
-        }
-    });
-
-    $('body').off('.acc_binding').on('keydown.acc_binding', function (event) {
-        if (event.altKey && event.shiftKey && event.key === 'F') {
-            toggleFontSize();
-            event.preventDefault();
-        }
-    });
+        });
 });
